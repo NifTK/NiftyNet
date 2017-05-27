fix normalisation and sampling

engine/training.py
@@ -58,7 +58,14 @@ def run(net_class, param, device_str):
                          cutoff=[x for x in param.norm_cutoff],
                          mask_type=param.mask_type)
     # define how to choose training volumes
-    volume_loader = VolumeLoaderLayer(csv_loader, hist_norm, do_shuffle=True)
+    volume_loader = VolumeLoaderLayer(csv_loader,
+                                      hist_norm,
+                                      do_shuffle=True,
+                                      do_reorientation=True,
+                                      do_resampling=True,
+                                      do_normalisation=True,
+                                      do_whitening=True,
+                                      interp_order=(3, 0))
     print('found {} subjects'.format(len(volume_loader.subject_list)))
 
 
@@ -99,11 +106,12 @@ def run(net_class, param, device_str):
         train_pairs = train_batch_runner.pop_batch_op
         images = train_pairs['images']
         labels = train_pairs['labels']
-        _ = net(images, is_training=True)
+        #_ = net(images, is_training=True)
         for i in range(param.num_gpus):
             with tf.device("/{}:{}".format(device_str, i)):
                 predictions = net(images, is_training=True)
                 loss = loss_func(predictions, labels)
+                # TODO compute miss for dfferent target types
                 miss = tf.reduce_mean(tf.cast(
                     tf.not_equal(tf.argmax(predictions, -1), labels[...,0]),
                     dtype=tf.float32))

layer/uniform_sampler.py
@@ -21,11 +21,6 @@ class UniformSampler(BaseSampler):
         self.volume_loader = volume_loader
         self.patch_per_volume = patch_per_volume
 
-        self.do_reorientation = True
-        self.do_resampling = True
-        self.do_normalisation = True
-        self.do_whitening = True
-
     def layer_op(self, batch_size=1):
         """
          problems:
@@ -42,10 +37,7 @@ class UniformSampler(BaseSampler):
         # N samples where (N % batch_size) == 0
 
         while self.volume_loader.has_next:
-            img, seg, _, idx = self.volume_loader(self.do_reorientation,
-                                                  self.do_resampling,
-                                                  self.do_normalisation,
-                                                  self.do_whitening)
+            img, seg, _, idx = self.volume_loader()
             if img.ndim == 5:
                 img = np.squeeze(img, axis=(4,))
             if seg.ndim == 5:

layer/volume_loader.py
@@ -1,6 +1,9 @@
-import numpy as np
 from random import shuffle
+
+import numpy as np
+
 from .base import Layer
+
 
 class VolumeLoaderLayer(Layer):
     """
@@ -16,13 +19,24 @@ class VolumeLoaderLayer(Layer):
                  csv_reader,
                  standardisor,
                  do_shuffle=True,
+                 do_reorientation=True,
+                 do_resampling=True,
+                 do_normalisation=True,
+                 do_whitening=True,
+                 interp_order=(3, 0),
                  name='volume_loader'):
 
         super(VolumeLoaderLayer, self).__init__(name=name)
 
         self.csv_table = csv_reader
         self.standardisor = standardisor
+
         self.do_shuffle = do_shuffle
+        self.do_reorientation = do_reorientation
+        self.do_resampling = do_resampling
+        self.do_normalisation = do_normalisation
+        self.do_whitening = do_whitening
+        self.interp_order = interp_order
 
         self.subject_list = None
         self.current_id = None
@@ -36,15 +50,12 @@ class VolumeLoaderLayer(Layer):
         self.subject_list = self.csv_table.to_subject_list()
         if self.do_shuffle:
             shuffle(self.subject_list)
+        if not self.standardisor.is_ready(self.do_normalisation,
+                                          self.do_whitening):
+            self.standardisor.train_normalisation_ref(self.subject_list)
         self.current_id = -1
 
-
-    def layer_op(self,
-                 do_reorientation=False,
-                 do_resampling=False,
-                 do_normalisation=False,
-                 do_whitening=False,
-                 interp_order=(3, 0, 3)):
+    def layer_op(self):
         """
         Call this function to get the next subject's image data.
         """
@@ -54,19 +65,19 @@ class VolumeLoaderLayer(Layer):
         else:
             self.current_id = self.current_id + 1
         current_subject = self.subject_list[self.current_id]
-        #print current_subject
+        # print current_subject
         subject_dict = current_subject.load_columns((0, 1, 2),
-                                                    do_reorientation,
-                                                    do_resampling,
-                                                    interp_order)
+                                                    self.do_reorientation,
+                                                    self.do_resampling,
+                                                    self.interp_order)
 
         image = subject_dict['input_image_file']
         label = subject_dict['target_image_file']
         weight = subject_dict['weight_map_file']
 
-        if not self.standardisor.is_ready(do_normalisation, do_whitening):
-            self.standardisor.train_normalisation_ref(self.subject_list)
-        image = self.standardisor(image, do_normalisation, do_whitening)
+        image = self.standardisor(image,
+                                  self.do_normalisation,
+                                  self.do_whitening)
         return image, label, weight, self.current_id
 
     @property
@@ -80,25 +91,3 @@ class VolumeLoaderLayer(Layer):
 
     def num_modality(self, column_id):
         return self.subject_list[0].column(column_id).num_modality
-
-
-        # def normalise_subject_data_and_save(self, subject):
-        #    if self.flags.flag_standardise:
-        #        data_dict = subject.read_all_modalities(self.flags.flag_reorient,
-        #                                                self.flags.flag_resample)
-        #        data_dict.input = np.nan_to_num(data_dict.input)
-        #        mask_array = self.make_mask_array(data_dict.input)
-        #        data_dict.input = self.standardisor.normalise_data_array(
-        #            data_dict.input, mask_array)
-        #        name_norm_save = io.create_new_filename(
-        #            subject.name + '.nii.gz',
-        #            new_path=self.dict_normalisation.path_to_save,
-        #            new_prefix='Norm')
-        #        # Put back the array with the nifti conventions.
-        #        data_nifti_format = np.swapaxes(data_dict.input, 4, 3)
-        #        io.save_img(data_nifti_format, subject.name, [], name_norm_save,
-        #                    filename_ref=subject.file_path_list.input.filename_ref,
-        #                    flag_orientation=self.flags.flag_reorient,
-        #                    flag_isotropic=self.flags.flag_resample)
-        #        # TODO: save norm
-        #        #subject._set_data_path(name_norm_save)

testing/volume_reader_test.py
@@ -40,21 +40,13 @@ class SubjectTest(tf.test.TestCase):
 
         volume_loader = VolumeLoaderLayer(csv_loader, hist_norm)
 
-        img, seg, weight_map, subject = volume_loader(
-            do_reorientation=True,
-            do_resampling=True,
-            do_normalisation=True,
-            do_whitening=True)
+        img, seg, weight_map, subject = volume_loader()
         print img.shape
         if seg is not None:
             print seg.shape
         print weight_map
         print volume_loader.subject_list[subject]
-        img, seg, weight_map, subject = volume_loader(
-            do_reorientation=False,
-            do_resampling=True,
-            do_normalisation=False,
-            do_whitening=True)
+        img, seg, weight_map, subject = volume_loader()
 
 
 if __name__ == "__main__":