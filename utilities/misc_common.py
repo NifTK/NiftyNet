6549f0a64089b40229432b2dc58812e9945b6acf
Wenqi Li <wenqi.li@ucl.ac.uk>
2017-06-08 15:23:10 +0100

    mean value option and progress bar for histogram norm.

--- utilities/misc_common.py
+++ utilities/misc_common.py
@@ -160,3 +160,26 @@ def _damerau_levenshtein_distance(s1, s2):
                                 d[i - 2, j - 2] + cost)  # transposition
 
     return d[string_1_length - 1, string_2_length - 1]
+
+# Print iterations progress
+def printProgressBar(iteration, total,
+                     prefix='', suffix='', decimals=1, length=10, fill = '='):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(
+        100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
