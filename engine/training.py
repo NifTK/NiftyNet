fix reg_loss,update config for min_percentage scaling
	modified:   config/deepmedic_config.txt
	modified:   config/default_config.txt
	modified:   config/default_multimodal_config.txt
	modified:   config/highres3dnet_config.txt
	modified:   config/scalenet_config.txt
	modified:   config/unet_config.txt
	modified:   config/vnet_config.txt
	modified:   engine/training.py
	modified:   layer/rand_spatial_scaling.py

engine/training.py
@@ -131,12 +131,13 @@ def run(net_class, param, csv_dict, device_str):
         for i in range(0, max(param.num_gpus, 1)):
             with tf.device("/{}:{}".format(device_str, i)):
                 predictions = net(images, is_training=True)
-                data_loss = loss_func(predictions, labels, weight_maps)
-                reg_losses = graph.get_collection(
-                    tf.GraphKeys.REGULARIZATION_LOSSES)
-                reg_loss = tf.reduce_mean([tf.reduce_mean(reg_loss)
-                                           for reg_loss in reg_losses])
-                loss = data_loss + reg_loss
+                loss = loss_func(predictions, labels, weight_maps)
+                if param.decay > 0:
+                    reg_losses = graph.get_collection(
+                        tf.GraphKeys.REGULARIZATION_LOSSES)
+                    reg_loss = tf.reduce_mean([tf.reduce_mean(reg_loss)
+                                               for reg_loss in reg_losses])
+                    loss = loss + reg_loss
                 # TODO compute miss for dfferent target types
                 miss = tf.reduce_mean(tf.cast(
                     tf.not_equal(tf.argmax(predictions, -1), labels[..., 0]),

layer/rand_spatial_scaling.py
@@ -23,9 +23,8 @@ class RandomSpatialScalingLayer(Layer):
                  name='random_spatial_scaling'):
         super(RandomSpatialScalingLayer, self).__init__(name=name)
         assert min_percentage < max_percentage
-        assert min_percentage > -100.0
-        self._min_percentage = int(min_percentage)
-        self._max_percentage = int(max_percentage)
+        self._min_percentage = max(min_percentage, -99.9)
+        self._max_percentage = max_percentage
         self._rand_zoom = None
 
     def randomise(self, spatial_rank=3):
@@ -33,7 +32,7 @@ class RandomSpatialScalingLayer(Layer):
         rand_zoom = np.random.uniform(low=self._min_percentage,
                                       high=self._max_percentage,
                                       size=(spatial_rank,))
-        self._rand_zoom = max((rand_zoom + 100.0) / 100.0, 1e-5)
+        self._rand_zoom = (rand_zoom + 100.0) / 100.0
 
     def _apply_transformation(self, image, interp_order=3):
         assert self._rand_zoom is not None