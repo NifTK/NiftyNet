2ca006a21cc4b3a923c679da15e0fff0e21ae80f
Eli GIBSON <egibson@cs.ucl.ac.uk>
2017-06-27 20:12:10 +0100

    Code to enable NN to read other file types with support for SimpleITK loading if installed

--- data/PROMISE12/setup.py
+++ data/PROMISE12/setup.py
@@ -6,19 +6,3 @@ for zip_filename in {'TrainingData_Part1.zip','TrainingData_Part1.zip','Training
   zip_ref = zipfile.ZipFile(os.path.join(zip_dir,zip_filename), 'r')
   zip_ref.extractall(target_dir)
   zip_ref.close()
-
-import SimpleITK
-import glob
-for fn in glob.glob('*.mhd'):
-  if 'segmentation' in fn:
-    fn_out=fn[:-4]+'.nii.gz'
-  else:
-    fn_out=fn[:-4]+'_T2.nii.gz'
-  SimpleITK.WriteImage(SimpleITK.ReadImage(fn),fn_out)
-  try:
-    os.remove(fn)
-    os.remove(fn[:-4]+'.raw')
-  except:
-    pass
-
-
