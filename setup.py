08e7b3be9a914c8fa233275b17e809449660173e
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-07-07 15:48:41 +0100

    Issue #69: loading long description from added RST file instead of hard-coding it

--- setup.py
+++ setup.py
@@ -1,30 +1,13 @@
 from setuptools import setup, find_packages
 
 
-more='NiftyNet was developed by the Centre for Medical Image' \
-     ' Computing at University College London (UCL).' \
-     '\n' \
-     'Features:' \
-     '\n * Easy-to-customise interfaces of network components' \
-     '\n * Designed for sharing networks and pretrained models' \
-     '\n * Designed to support 2-D, 2.5-D, 3-D, 4-D inputs (' \
-     '\n   (2.5-D: volumetric images processed as a stack of 2D' \
-     ' slices; 4-D: co-registered multi-modal 3D volumes)' \
-     '\n * Efficient discriminative training with multiple-GPU' \
-     ' support' \
-     '\n * Implemented recent networks (HighRes3DNet, 3D U-net,' \
-     ' V-net, DeepMedic)' \
-     '\n * Comprehensive evaluation metrics for medical image' \
-     ' segmentation'
-
-
 setup(
     name='NiftyNet',
 
     version='0.1',  # TODO
 
     description=open('pip/description.rst').read(),
-    long_description=more,  # TODO
+    long_description=open('pip/long_description.rst').read(),
 
     url='',  # TODO
 
