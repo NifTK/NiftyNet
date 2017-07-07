ae8d89c4a1b2fd8c5fa9b98fec08c1092f8b73e5
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-07-07 14:17:10 +0100

    Issue #69: added skeletal setup.py that just does a dummy installation

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='NiftyNet',
+
+    version='0.1',  # TODO
+
+    description='',  # TODO
+    long_description='',  # TODO
+
+    url='',  # TODO
+
+    author='',  # TODO
+    author_email='',  # TODO
+
+    license='',  # TODO
+
+    classifiers=[
+        '',
+
+        '',
+    ],  # TODO
+
+    keywords='',  # TODO
+
+    packages=find_packages(exclude=['', '', '']),  # TODO
+
+    install_requires=['', '', ''],  # TODO
+
+    entry_points={
+        'console_scripts': [
+            '',  # TODO
+        ],
+    },
+)
+
