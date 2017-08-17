412b8e7240e52be802bc6fcd0eecafb51dad8c03
Tom Doel <t.doel@ucl.ac.uk>
2017-08-17 11:46:16 +0100

    Add a function to return a user-visible version number, which will either be the pip installed version, or the git repository local version number

---
+++ niftynet/utilities/get_niftynet_version.py
@@ -0,0 +1,23 @@
+# noinspection PyBroadException
+def get_niftynet_version():
+    """Return a user-visible string describing the product version"""
+
+    try:
+        from subprocess import check_output, CalledProcessError
+        # Describe the version relative to last tag
+        command_git = ['git', 'describe', '--match', 'v[0-9]*']
+        version_string = check_output(command_git).decode().rstrip()
+
+        # Exclude the 'v' for PEP440 conformity, see
+        # https://www.python.org/dev/peps/pep-0440/#public-version-identifiers
+        version_string = version_string[1:]
+
+    except:
+        try:
+            import pkg_resources
+            version_string = pkg_resources.get_distribution("niftynet").version
+
+        except:
+            version_string = "unknown"
+
+    return "NiftyNet version " + version_string
