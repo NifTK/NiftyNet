7e3d6f87d89c0556571c6b3a84512b1b1e56512f
Tom Doel <t.doel@ucl.ac.uk>
2017-08-18 22:54:10 +0100

    Added download utility

---
+++ niftynet/utilities/download.py
@@ -0,0 +1,270 @@
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+
+import tarfile
+import tempfile
+from shutil import copyfile
+
+import requests
+import six
+from six.moves.urllib.request import urlretrieve
+from six.moves.urllib.parse import urlparse
+from os.path import basename
+
+import argparse
+import os
+
+from six.moves.configparser import SafeConfigParser
+
+from os.path import expanduser
+from distutils.version import LooseVersion
+
+from utilities.get_niftynet_version import get_niftynet_version, get_niftynet_version_string
+
+
+def download(example_id, niftynet_base_folder=None, download_if_already_existing=False):
+    """
+    Downloads standard NiftyNet examples such as data, samples
+
+    :param example_id: The identifier for the sample
+    :param niftynet_base_folder: The base folder where downloads are stored
+    :param download_if_already_existing: If true, data will always be downloaded
+    """
+
+    if not niftynet_base_folder:
+        niftynet_base_folder = os.path.join(expanduser("~"), 'niftynet')
+
+    remote_base_url = 'https://cmiclab.cs.ucl.ac.uk/CMIC/NiftyNetExamples/raw/master/'
+
+    config_store = ConfigStore(niftynet_base_folder, remote_base_url)
+
+    if not config_store.exists(example_id):
+        print('No NiftyNet example was found with the name ' + example_id)
+        return
+
+    config_store.update_if_required(example_id, download_if_already_existing)
+
+
+def download_file(url, download_path):
+    """
+    Download a file from a resource URL to the given location
+
+    :param url: URL of the file to download
+    :param download_path: location where the file should be saved
+    """
+
+    # Extract the filename from the URL
+    parsed = urlparse(url)
+    filename = basename(parsed.path)
+
+    # Ensure the output directory exists
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    # Get a temporary file path for the compressed file download
+    downloaded_file = os.path.join(tempfile.gettempdir(), filename)
+
+    # Download the file
+    urlretrieve(url, downloaded_file)
+
+    # Move the file to the destination folder
+    destination_path = os.path.join(download_path, filename)
+    os.rename(downloaded_file, destination_path)
+
+
+def download_and_decompress(url, download_path):
+    """
+    Download an archive from a resource URL and decompresses/unarchives to the given location
+
+    :param url: URL of the compressed file to download
+    :param download_path: location where the file should be extracted
+    """
+
+    # Extract the filename from the URL
+    parsed = urlparse(url)
+    filename = basename(parsed.path)
+
+    # Ensure the output directory exists
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    # Get a temporary file path for the compressed file download
+    downloaded_file = os.path.join(tempfile.gettempdir(), filename)
+
+    # Download the file
+    urlretrieve(url, downloaded_file)
+
+    # Decompress and extract all files to the specified local path
+    tar = tarfile.open(downloaded_file, "r")
+    tar.extractall(download_path)
+    tar.close()
+
+    # Remove the downloaded file
+    os.remove(downloaded_file)
+
+
+class ConfigStore:
+    """Manages a configuration file store based on a remote repository with local caching"""
+
+    def __init__(self, parent_store_folder, remote_base_url):
+        self._parent_store_folder = parent_store_folder
+        self._local = ConfigStoreCache(os.path.join(parent_store_folder, '.downloads_local_config_cache'))
+        self._remote = RemoteProxy(parent_store_folder, remote_base_url)
+
+    def exists(self, example_id):
+        """Returns True if a record exists for this example_id, either locally or remotely"""
+
+        return self._local.exists(example_id) or self._remote.exists(example_id)
+
+    def update_if_required(self, example_id, download_if_already_existing=False):
+        """Downloads data using the configuraiton file if it is not already up to date"""
+
+        self._remote.update(example_id)
+        current_config, current_entries = self._local.get_download_params(example_id)
+        remote_config, remote_entries = self._remote.get_download_params(example_id)
+        if download_if_already_existing or self._is_update_required(current_config, remote_config):
+            self._check_minimum_niftynet_version(remote_config)
+            self._download(remote_entries)
+            self._replace_local_with_remote_config(example_id)
+        else:
+            print("Examples are already downloaded")
+            print("Use the -r option to force the data to be downloaded again")
+
+    @staticmethod
+    def _check_minimum_niftynet_version(remote_config):
+        if 'min_niftynet' in remote_config:
+            min_niftynet = remote_config['min_niftynet']
+            current_version = get_niftynet_version()
+            if LooseVersion(min_niftynet) > LooseVersion(current_version):
+                raise ValueError("This example requires NiftyNet version " + min_niftynet + " or later.")
+
+    @staticmethod
+    def _is_update_required(current_config, remote_config):
+        if 'version' not in current_config:
+            return True
+        return LooseVersion(current_config['version']) < LooseVersion(remote_config['version'])
+
+    def _download(self, remote_config_sections):
+        for section_name, config_params in remote_config_sections.items():
+            if 'url' in config_params:
+                action = config_params.get('action', 'unarchive').lower()
+                if action == 'unarchive':
+                    local_download_path = self._get_local_download_path(config_params)
+                    download_and_decompress(url=config_params['url'], download_path=local_download_path)
+                    print("Downloaded data to " + local_download_path)
+                else:
+                    raise ValueError("I am unable to download example as I do not understand the action " +
+                                     config_params['action'] + ". Perhaps you need to update to a newer version of "
+                                                               "NiftyNet?")
+
+    def _get_local_download_path(self, remote_config):
+        return os.path.join(self._parent_store_folder, remote_config['destination'], remote_config['local_id'])
+
+    def _replace_local_with_remote_config(self, example_id):
+        local_filename = self._local.get_local_path(example_id)
+        remote_filename = self._remote.get_local_path(example_id)
+        copyfile(remote_filename, local_filename)
+
+
+class ConfigStoreCache:
+    """A local cache for configuration files"""
+
+    def __init__(self, cache_folder):
+        self._cache_folder = cache_folder
+        if not os.path.exists(self._cache_folder):
+            os.makedirs(self._cache_folder)
+
+    def exists(self, example_id):
+        """Returns True if a record exists for this example_id, either locally or remotely"""
+
+        return os.path.isfile(self.get_local_path(example_id))
+
+    def get_local_path(self, example_id):
+        """Returns the full path to the locally cached configuration file"""
+
+        return os.path.join(self._cache_folder, example_id + '.ini')
+
+    def get_local_cache_folder(self):
+        """Returns the folder in which the cached files are stored"""
+
+        return self._cache_folder
+
+    def get_download_params(self, example_id):
+        """Returns the local configuration file for this example_id"""
+
+        config_filename = self.get_local_path(example_id)
+
+        parser = SafeConfigParser()
+        parser.read(config_filename)
+        config_section = dict(parser.items('config')) if 'config' in parser else {}
+        other_sections = {key: value for key, value in parser.items() if value != 'config' and value != 'DEFAULT'}
+        return config_section, other_sections
+
+
+class RemoteProxy:
+    """A remote configuration file store with a local cache"""
+
+    def __init__(self, parent_store_folder, base_url):
+        self._cache = ConfigStoreCache(os.path.join(parent_store_folder, '.downloads_remote_config_cache'))
+        self._remote = RemoteConfigStore(base_url)
+
+    def exists(self, example_id):
+        """Returns True if a record exists locally or remotely"""
+
+        return self._cache.exists(example_id) or self._remote.exists(example_id)
+
+    def update(self, example_id):
+        """Retrieves the latest record from the remote store and puts locally into the remote cache"""
+
+        download_file(self._remote.get_url(example_id), self._cache.get_local_cache_folder())
+
+    def get_download_params(self, example_id):
+        """Returns the local configuration file for this example_id"""
+
+        return self._cache.get_download_params(example_id)
+
+    def get_local_path(self, example_id):
+        """Returns the full path to the locally cached configuration file"""
+
+        return self._cache.get_local_path(example_id)
+
+
+class RemoteConfigStore:
+    """A remote configuration file store"""
+
+    def __init__(self, base_url):
+        self._base_url = base_url
+
+    def exists(self, example_id):
+        """Returns true if the record exists on the remote server"""
+
+        return self._url_exists(self.get_url(example_id))
+
+    def get_url(self, example_id):
+        """Gets the URL for the record for this example_id"""
+
+        return six.moves.urllib.parse.urljoin(self._base_url, example_id + '.ini')
+
+    @staticmethod
+    def _url_exists(url):
+        """Returns true if the specified url exists, without any redirects"""
+
+        resp = requests.head(url)
+        return resp.status_code == 200
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(description="Download NiftyNet sample data")
+    arg_parser.add_argument("-s", "--sample_id", help="Identifier string for the sample to download")
+    arg_parser.add_argument("-r", "--retry", help="Force data to be downloaded again", required=False,
+                            action='store_true')
+    arg_parser.add_argument("-d", "--data_folder", help="Change data download location", required=False,
+                            default=None)
+    version_string = get_niftynet_version_string()
+    arg_parser.add_argument("-v", "--version", action='version', version=version_string)
+    args = arg_parser.parse_args()
+
+    download(args.sample_id, args.data_folder, args.retry)
+
+if __name__ == "__main__":
+    main()
