052dd896f9cfe60dd4e18549c139d6b6764f0f53
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-09-05 14:24:14 +0100

    Issue #111: added one-liner summary to niftynet module

--- niftynet/__init__.py
+++ niftynet/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Entry points for the NiftyNet CLI."""
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
