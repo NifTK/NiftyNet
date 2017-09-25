2d50296132dcbf32b5375b921fbc9671760d2b7b
Wenqi Li <wenqi.li@ucl.ac.uk>
2017-09-25 15:02:24 +0100

    demo app of learning rate decaying

---
+++ niftynet/contrib/learning_rate_schedule/decay_lr_application.py
@@ -0,0 +1,96 @@
+import tensorflow as tf
+
+from niftynet.application.segmentation_application import SegmentationApplication
+from niftynet.engine.application_factory import ApplicationNetFactory
+from niftynet.engine.application_factory import OptimiserFactory
+from niftynet.engine.application_variables import CONSOLE
+from niftynet.engine.application_variables import NETWORK_OUTPUT
+from niftynet.engine.application_variables import TF_SUMMARIES
+from niftynet.engine.windows_aggregator_grid import GridSamplesAggregator
+from niftynet.engine.windows_aggregator_resize import ResizeSamplesAggregator
+from niftynet.engine.sampler_grid import GridSampler
+from niftynet.engine.sampler_resize import ResizeSampler
+from niftynet.engine.sampler_uniform import UniformSampler
+from niftynet.io.image_reader import ImageReader
+from niftynet.layer.binary_masking import BinaryMaskingLayer
+from niftynet.layer.discrete_label_normalisation import \
+    DiscreteLabelNormalisationLayer
+from niftynet.layer.histogram_normalisation import \
+    HistogramNormalisationLayer
+from niftynet.layer.loss_segmentation import LossFunction
+from niftynet.layer.mean_variance_normalisation import \
+    MeanVarNormalisationLayer
+from niftynet.layer.pad import PadLayer
+from niftynet.layer.post_processing import PostProcessingLayer
+from niftynet.layer.rand_flip import RandomFlipLayer
+from niftynet.layer.rand_rotation import RandomRotationLayer
+from niftynet.layer.rand_spatial_scaling import RandomSpatialScalingLayer
+
+SUPPORTED_INPUT = {'image', 'label', 'weight'}
+
+
+class DecayLearningRateApplication(SegmentationApplication):
+    REQUIRED_CONFIG_SECTION = "SEGMENTATION"
+
+    def __init__(self, net_param, action_param, is_training):
+        SegmentationApplication.__init__(
+            self, net_param, action_param, is_training)
+        tf.logging.info('starting decay learning segmentation application')
+        self.learning_rate = None
+
+    def connect_data_and_network(self,
+                                 outputs_collector=None,
+                                 gradients_collector=None):
+        data_dict = self.get_sampler()[0].pop_batch_op()
+        image = tf.cast(data_dict['image'], tf.float32)
+        net_out = self.net(image, self.is_training)
+
+        if self.is_training:
+            with tf.name_scope('Optimiser'):
+                self.learning_rate = tf.placeholder(tf.float32, shape=[])
+                optimiser_class = OptimiserFactory.create(
+                    name=self.action_param.optimiser)
+                self.optimiser = optimiser_class.get_instance(
+                    learning_rate=learning_rate)
+            loss_func = LossFunction(
+                n_class=self.segmentation_param.num_classes,
+                loss_type=self.action_param.loss_type)
+            data_loss = loss_func(
+                prediction=net_out,
+                ground_truth=data_dict.get('label', None),
+                weight_map=data_dict.get('weight', None))
+            if self.net_param.decay > 0.0:
+                reg_losses = tf.get_collection(
+                    tf.GraphKeys.REGULARIZATION_LOSSES)
+                if reg_losses:
+                    reg_loss = tf.reduce_mean(
+                        [tf.reduce_mean(reg_loss) for reg_loss in reg_losses])
+                    loss = data_loss + reg_loss
+            else:
+                loss = data_loss
+            grads = self.optimiser.compute_gradients(loss)
+            # collecting gradients variables
+            gradients_collector.add_to_collection([grads])
+            # collecting output variables
+            outputs_collector.add_to_collection(
+                var=data_loss, name='dice_loss',
+                average_over_devices=False, collection=CONSOLE)
+            outputs_collector.add_to_collection(
+                var=data_loss, name='dice_loss',
+                average_over_devices=True, summary_type='scalar',
+                collection=TF_SUMMARIES)
+        else:
+            # converting logits into final output for
+            # classification probabilities or argmax classification labels
+            SegmentationApplication.connect_data_and_network(
+                self, outputs_collector, gradients_collector)
+
+    def training_ops(self, start_iter=0, end_iter=1):
+        if start_iter > end_iter:
+            start_iter, end_iter = end_iter, start_iter
+        current_lr = self.action_param.lr
+        for iter_i in range(start_iter, end_iter):
+            if iter_i % 10 == 0:
+                # halved every 10 iteration
+                current_lr = self.action_param.lr / 2.0
+            yield iter_i, self.gradient_op[0], {self.learning_rate, current_lr}
