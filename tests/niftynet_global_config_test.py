971568fcd3e682943a040d1a1a3c5d84d91c3178
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-10-12 11:50:33 +0100

    Issue #181: added a test suite for global config that includes a singleton test currently

---
+++ tests/niftynet_global_config_test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from niftynet.utilities.niftynet_global_config import NiftyNetGlobalConfig
+
+
+class NiftyNetGlobalConfigTest(TestCase):
+
+    def test_global_config_singleton(self):
+        global_config_1 = NiftyNetGlobalConfig()
+        global_config_2 = NiftyNetGlobalConfig()
+        self.assertEqual(global_config_1, global_config_2)
