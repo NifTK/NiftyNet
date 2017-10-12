4951e6fca457188df72101bfd61b10bc0a96a0a5
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-10-12 14:56:38 +0100

    Revert "Issue #181: added dummy testing class to NiftyNetGlobalConfigTest"
    This reverts commit 26059791f959e02722549116cbae171fbb7cf830.

--- tests/niftynet_global_config_test.py
+++ tests/niftynet_global_config_test.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from niftynet.utilities.niftynet_global_config import NiftyNetGlobalConfig
-from niftynet.utilities.decorators import singleton
 
 
 class NiftyNetGlobalConfigTest(TestCase):
@@ -9,16 +8,3 @@ class NiftyNetGlobalConfigTest(TestCase):
         global_config_1 = NiftyNetGlobalConfig()
         global_config_2 = NiftyNetGlobalConfig()
         self.assertEqual(global_config_1, global_config_2)
-
-
-@singleton
-class Dummy(object):
-    def __init__(self):
-        pass
-
-
-if __name__ == '__main__':
-    dummy_1 = Dummy()
-    dummy_2 = Dummy()
-    assert dummy_1 is dummy_2
-    assert dummy_1 == dummy_2
