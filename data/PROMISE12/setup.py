899de963ae3449e76fc2e8245a573d1b2cee0027
Joeri Nicolaes <joerinicolaes@gmail.com>
2017-10-30 15:36:45 +0100

    minor fix to move zipped files into separate folders for part1, 2 and 3

--- data/PROMISE12/setup.py
+++ data/PROMISE12/setup.py
@@ -11,8 +11,8 @@ import zipfile
 
 zip_dir = '.'
 target_dir = '.'
-for zip_filename in {'TrainingData_Part1.zip', 'TrainingData_Part1.zip',
-                     'TrainingData_Part1.zip'}:
+for zip_filename in {'TrainingData_Part1.zip', 'TrainingData_Part2.zip',
+                     'TrainingData_Part3.zip'}:
     zip_ref = zipfile.ZipFile(os.path.join(zip_dir, zip_filename), 'r')
-    zip_ref.extractall(target_dir)
+    zip_ref.extractall(os.path.join(target_dir,zip_filename[:-4]))
     zip_ref.close()
