2676914fcd53bb96a38e45c279b6d7bb4194926f
Wenqi Li <wenqi.li@ucl.ac.uk>
2017-11-30 13:48:51 +0000

    moving brain parcellation demo to the zoo

--- data/brain_parcellation/get_oasis_data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# Download a demo volume extracted from OASIS dataset.
-
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "$DIR"
-wget -c https://www.dropbox.com/s/zpk0mom7aoawpyq/OASIS_DATA.tar.gz
-tar -xvf OASIS_DATA.tar.gz
