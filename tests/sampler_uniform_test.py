90e155b7bdea91c618e07ec038ede238e64a758e
Wenqi Li <wenqi.li@ucl.ac.uk>
2018-04-06 11:43:06 +0100

    concentric windows test

--- tests/sampler_uniform_test.py
+++ tests/sampler_uniform_test.py
@@ -39,6 +39,32 @@ MULTI_MOD_DATA = {
 }
 MULTI_MOD_TASK = ParserNamespace(image=('T1', 'FLAIR'))
 
+MULTI_WINDOW_DATA = {
+    'T1': ParserNamespace(
+        csv_file=os.path.join('testing_data', 'T1sampler.csv'),
+        path_to_search='testing_data',
+        filename_contains=('_o_T1_time',),
+        filename_not_contains=('Parcellation',),
+        interp_order=3,
+        pixdim=None,
+        axcodes=None,
+        spatial_window_size=(4, 10, 3),
+        loader=None
+    ),
+    'FLAIR': ParserNamespace(
+        csv_file=os.path.join('testing_data', 'FLAIRsampler.csv'),
+        path_to_search='testing_data',
+        filename_contains=('FLAIR_',),
+        filename_not_contains=('Parcellation',),
+        interp_order=3,
+        pixdim=None,
+        axcodes=None,
+        spatial_window_size=(7, 12, 2),
+        loader=None
+    )
+}
+MULTI_WINDOW_TASK = ParserNamespace(image=('T1',), label=('FLAIR',))
+
 MOD_2D_DATA = {
     'ultrasound': ParserNamespace(
         csv_file=os.path.join('testing_data', 'T1sampler2d.csv'),
@@ -103,8 +129,32 @@ def get_dynamic_window_reader():
     reader.initialise(DYNAMIC_MOD_DATA, DYNAMIC_MOD_TASK, dynamic_list)
     return reader
 
+def get_concentric_window_reader():
+    reader = ImageReader(['image', 'label'])
+    reader.initialise(MULTI_WINDOW_DATA, MULTI_WINDOW_TASK, multi_mod_list)
+    return reader
+
 
 class UniformSamplerTest(tf.test.TestCase):
+    def test_3d_concentric_init(self):
+        sampler = UniformSampler(reader=get_concentric_window_reader(),
+                                 data_param=MULTI_WINDOW_DATA,
+                                 batch_size=2,
+                                 windows_per_image=10,
+                                 queue_length=10)
+        with self.test_session() as sess:
+            coordinator = tf.train.Coordinator()
+            sampler.run_threads(sess, coordinator, num_threads=2)
+            out = sess.run(sampler.pop_batch_op())
+            img_loc = out['image_location']
+            seg_loc = out['label_location']
+            self.assertTrue(np.all(img_loc[:, 0] == seg_loc[:, 0]))
+            self.assertTrue(np.all((img_loc - seg_loc)[:, 1:4] == [1, 1, 0]))
+            self.assertTrue(np.all((img_loc - seg_loc)[:, 4:] == [-2, -1, 1]))
+            self.assertAllClose(out['image'].shape, (2, 4, 10, 3, 1))
+            self.assertAllClose(out['label'].shape, (2, 7, 12, 2, 1))
+        sampler.close_all()
+
     def test_3d_init(self):
         sampler = UniformSampler(reader=get_3d_reader(),
                                  data_param=MULTI_MOD_DATA,
@@ -165,7 +215,7 @@ class RandomCoordinatesTest(tf.test.TestCase):
     def assertCoordinatesAreValid(self, coords, img_size, win_size):
         for coord in coords:
             for i in range(len(coord.shape)):
-                self.assertTrue(coord[i] >= 0)
+                self.assertTrue(coord[i] >= int(win_size[i] / 2))
                 self.assertTrue(coord[i] <= img_size[i] - int(win_size[i]/2))
 
     def test_3d_coordinates(self):
@@ -187,11 +237,12 @@ class RandomCoordinatesTest(tf.test.TestCase):
 
     def test_1d_coordinates(self):
         cropped_map=np.zeros((1, 1, 1))
-        img_size = [8, 1, 1]
+        img_size = [4, 1, 1]
         win_size = [2, 1, 1]
         coords = rand_spatial_coordinates(
-            10, img_size, win_size, None)
-        self.assertAllEqual(coords.shape, (10, N_SPATIAL))
+            20, img_size, win_size, None)
+        print(coords)
+        self.assertAllEqual(coords.shape, (20, N_SPATIAL))
         self.assertCoordinatesAreValid(coords, img_size, win_size)
 
 
