48c7c9bc4f50a16154d0146262e1acc05ae27d92
Zach E-R <z.eaton-rosen@ucl.ac.uk>
2018-04-23 17:14:53 +0100

    randomness of elastic augmentation now set in .randomise method

--- niftynet/contrib/layer/rand_elastic_deform.py
+++ niftynet/contrib/layer/rand_elastic_deform.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-#Data augmentation using elastic deformations as used by:
-#Milletari,F., Navab, N., & Ahmadi, S. A. (2016) V-net:
-#Fully convolutional neural networks for volumetric medical
-#image segmentation
+# Data augmentation using elastic deformations as used by:
+# Milletari,F., Navab, N., & Ahmadi, S. A. (2016) V-net:
+# Fully convolutional neural networks for volumetric medical
+# image segmentation
 
 
 from __future__ import absolute_import, print_function
@@ -28,15 +28,31 @@ class RandomElasticDeformationLayer(RandomisedLayer):
     def __init__(self,
                  num_controlpoints=4,
                  std_deformation_sigma=15,
-                 name='random_elastic_deformation'):
+                 name='random_elastic_deformation',
+                 proportion_to_augment=0.5):
+        """
+        This layer elastically deforms the inputs, for data-augmentation purposes.
+        :param num_controlpoints:
+        :param std_deformation_sigma:
+        :param name: name for tensorflow graph
+        :param proportion_to_augment: what fraction of the images to do augmentation on
+        (may be computationally expensive).  
+        """
+
         super(RandomElasticDeformationLayer, self).__init__(name=name)
 
         self.num_controlpoints = max(num_controlpoints, 2)
         self.std_deformation_sigma = max(std_deformation_sigma, 1)
+        self.proportion_to_augment = proportion_to_augment
+        self.do_augmentation = None
         self.bspline_transformation = None
 
     def randomise(self, image_dict, spatial_rank=3):
-        images = image_dict.values()
+        self.do_augmentation = np.random.rand() < self.proportion_to_augment
+        if not self.do_augmentation:
+            pass
+
+        images = list(image_dict.values())
         equal_shapes = np.all([images[0].shape == image.shape for image in images])
         if spatial_rank == 3 and equal_shapes:
             self._randomise_bspline_transformation_3d(images[0].shape)
@@ -48,46 +64,44 @@ class RandomElasticDeformationLayer(RandomisedLayer):
     def _randomise_bspline_transformation_3d(self, shape):
         # generate transformation
         itkimg = sitk.GetImageFromArray(np.zeros(shape[:3]))
-        transfromDomainMeshSize = [self.num_controlpoints] * itkimg.GetDimension()
-        self.bspline_transformation = sitk.BSplineTransformInitializer(itkimg, transfromDomainMeshSize)
+        trans_from_domain_mesh_size = [self.num_controlpoints] * itkimg.GetDimension()
+        self.bspline_transformation = sitk.BSplineTransformInitializer(itkimg, trans_from_domain_mesh_size)
 
         params = self.bspline_transformation.GetParameters()
-        paramsNp = np.asarray(params, dtype=float)
-        paramsNp = paramsNp + np.random.randn(paramsNp.shape[0]) * self.std_deformation_sigma
+        params_numpy = np.asarray(params, dtype=float)
+        params_numpy = params_numpy + np.random.randn(params_numpy.shape[0]) * self.std_deformation_sigma
 
-        # paramsNp[0:int(len(params) / 3)] = 0  # remove z deformations! The resolution in z is too bad
+        # params_numpy[0:int(len(params) / 3)] = 0  # remove z deformations! The resolution in z is too bad
 
-        params = tuple(paramsNp)
+        params = tuple(params_numpy)
         self.bspline_transformation.SetParameters(params)
 
     def _apply_bspline_transformation_3d(self, image, interp_order=3):
 
-        if (np.random.rand(1)[0] > 0.5):  # do not apply deformations always, just sometimes
-            sitkImage = sitk.GetImageFromArray(image)
-            resampler = sitk.ResampleImageFilter()
-            resampler.SetReferenceImage(sitkImage)
-            if interp_order == 3:
-                resampler.SetInterpolator(sitk.sitkBSpline)
-            elif interp_order == 2:
-                resampler.SetInterpolator(sitk.sitkLinear)
-            elif interp_order == 1 or interp_order == 0:
-                resampler.SetInterpolator(sitk.sitkNearestNeighbor)
-            else:
-                raise RuntimeError("not supported interpolation_order")
+        sitk_image = sitk.GetImageFromArray(image)
+        resampler = sitk.ResampleImageFilter()
+        resampler.SetReferenceImage(sitk_image)
+        if interp_order == 3:
+            resampler.SetInterpolator(sitk.sitkBSpline)
+        elif interp_order == 2:
+            resampler.SetInterpolator(sitk.sitkLinear)
+        elif interp_order == 1 or interp_order == 0:
+            resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+        else:
+            raise RuntimeError("not supported interpolation_order")
 
-            resampler.SetDefaultPixelValue(0)
-            resampler.SetTransform(self.bspline_transformation)
-
-            outimgsitk = resampler.Execute(sitkImage)
-
-            outimg = sitk.GetArrayFromImage(outimgsitk)
-            return outimg
-
-        return image
+        resampler.SetDefaultPixelValue(0)
+        resampler.SetTransform(self.bspline_transformation)
+        out_img_sitk = resampler.Execute(sitk_image)
+        out_img = sitk.GetArrayFromImage(out_img_sitk)
+        return out_img
 
     def layer_op(self, inputs, interp_orders, *args, **kwargs):
 
         if inputs is None:
+            return inputs
+
+        if not self.do_augmentation:
             return inputs
 
         if isinstance(inputs, dict) and isinstance(interp_orders, dict):
