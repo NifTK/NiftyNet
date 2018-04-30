edd408de3f6f1727a76e33c5ea76d7704c88fe60
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-04-30 19:24:17 +0100

    Issue #212: using Python 3 for bundling the wheel

--- ci/bundlewheel.sh
+++ ci/bundlewheel.sh
@@ -4,7 +4,7 @@
 rm -f $niftynet_dir/dist/*.whl
 
 # bundle installer
-python setup.py bdist_wheel
+python3 setup.py bdist_wheel
 
 # inform other scripts of wheel's location
 export niftynet_wheel=$(ls $niftynet_dir/dist/*.whl)  # there will be only one file!
