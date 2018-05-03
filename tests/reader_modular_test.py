6e9591bb7df426709aaaa6d86823af4ab4dff0ae
Wenqi Li <wenqi.li@ucl.ac.uk>
2018-05-03 20:12:09 +0100

    reader: [x, y, z, 1, 1] (with spatial rank 3)

--- tests/reader_modular_test.py
+++ tests/reader_modular_test.py
@@ -50,13 +50,29 @@ def generate_2d_1d_images():
     return
 
 
+def generate_3d_1_1_d_images():
+    img_path = os.path.join(os.path.dirname(__file__), '..', 'testing_data')
+    img_path = os.path.realpath(os.path.join(img_path, 'images_x_y_z_1_1'))
+    if not os.path.isdir(img_path):
+        os.makedirs(img_path)
+    for idx in range(3):
+        image_data = np.random.randint(0, 255, size=(50, 24, 42, 1, 1))
+        image_data = image_data.astype(np.uint8)
+        nib_image = nib.Nifti1Image(image_data, np.eye(4))
+        nib.save(nib_image,
+                 os.path.join(img_path, 'x_y_z_1_1_{}.nii.gz'.format(idx)))
+    return
+
+
 generate_2d_images()
 generate_2d_1d_images()
+generate_3d_1_1_d_images()
 
 IMAGE_PATH_2D = os.path.join('.', 'testing_data', 'images2d')
 IMAGE_PATH_2D_1 = os.path.join('.', 'example_volumes', 'gan_test_data')
 IMAGE_PATH_2D_2 = os.path.join('.', 'testing_data', 'images_x_1_y')
 IMAGE_PATH_3D = os.path.join('.', 'testing_data')
+IMAGE_PATH_3D_1 = os.path.join('.', 'testing_data', 'images_x_y_z_1_1')
 
 
 class Read2DTest(tf.test.TestCase):
@@ -122,7 +138,7 @@ class Read2DTest(tf.test.TestCase):
 
     def test_input_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-            'csv_file': '2d_test.csv'}}
+                             'csv_file': '2d_test.csv'}}
         reader = ImageReader().initialise(data_param)
         self.default_property_asserts(reader)
         idx, data, interp = reader()
@@ -135,9 +151,9 @@ class Read2DTest(tf.test.TestCase):
 
     def test_no_2d_resampling_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-            'csv_file': '2d_test.csv',
-            'pixdim': (2, 2, 2),
-            'axcodes': 'RAS'}}
+                             'csv_file': '2d_test.csv',
+                             'pixdim': (2, 2, 2),
+                             'axcodes': 'RAS'}}
         reader = ImageReader().initialise(data_param)
         self.assertEqual(reader.output_list[0]['mr'].output_pixdim, (None,))
         self.assertEqual(reader.output_list[0]['mr'].output_axcodes, (None,))
@@ -151,18 +167,18 @@ class Read2DTest(tf.test.TestCase):
 
     def test_2d_as_5D_multimodal_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-            'filename_contains': '_u',
-            'pixdim': (2, 2, 2),
-            'axcodes': 'RAS'}}
+                             'filename_contains': '_u',
+                             'pixdim': (2, 2, 2),
+                             'axcodes': 'RAS'}}
         grouping_param = {'ct': ('mr', 'mr', 'mr')}
         reader = ImageReader().initialise(data_param, grouping_param)
         self.assertEqual(reader.spatial_ranks, {'ct': 2})
 
     def test_2D_multimodal_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-            'filename_contains': '_g',
-            'pixdim': (2, 1.5, 2),
-            'axcodes': 'RAS'}}
+                             'filename_contains': '_g',
+                             'pixdim': (2, 1.5, 2),
+                             'axcodes': 'RAS'}}
         grouping_param = {'ct': ('mr', 'mr', 'mr')}
         reader = ImageReader().initialise(data_param, grouping_param)
         self.assertDictEqual(reader.spatial_ranks, {'ct': 2})
@@ -183,10 +199,10 @@ class Read2D_1DTest(tf.test.TestCase):
     # loading 2d images of rank 3: [x, y, 1]
     def test_no_2d_resampling_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D_1,
-            'csv_file': '2d_test.csv',
-            'filename_contains': '_img',
-            'pixdim': (2, 2, 2),
-            'axcodes': 'RAS'}}
+                             'csv_file': '2d_test.csv',
+                             'filename_contains': '_img',
+                             'pixdim': (2, 2, 2),
+                             'axcodes': 'RAS'}}
         reader = ImageReader().initialise(data_param)
         self.assertEqual(reader.output_list[0]['mr'].output_pixdim,
                          ((2.0, 2.0, 2.0),))
@@ -202,9 +218,9 @@ class Read2D_1DTest(tf.test.TestCase):
 
     def test_2D_multimodal_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D_1,
-            'filename_contains': '_img',
-            'pixdim': (2, 1.5, 2),
-            'axcodes': 'RAS'}}
+                             'filename_contains': '_img',
+                             'pixdim': (2, 1.5, 2),
+                             'axcodes': 'RAS'}}
         grouping_param = {'ct': ('mr', 'mr', 'mr')}
         reader = ImageReader().initialise(data_param, grouping_param)
         self.assertDictEqual(reader.spatial_ranks, {'ct': 2})
@@ -220,13 +236,14 @@ class Read2D_1DTest(tf.test.TestCase):
         self.assertDictEqual(interp, {'ct': (1, 1, 1)})
         self.assertEqual(data['ct'].shape, (120, 160, 1, 1, 3))
 
+
 class Read2D_1D_x1y_Test(tf.test.TestCase):
     # loading 2d images of rank 3: [x, 1, y]
     def test_no_2d_resampling_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D_2,
-            'filename_contains': 'x_1_y',
-            'pixdim': (2, 2, 2),
-            'axcodes': 'RAS'}}
+                             'filename_contains': 'x_1_y',
+                             'pixdim': (2, 2, 2),
+                             'axcodes': 'RAS'}}
         reader = ImageReader().initialise(data_param)
         self.assertEqual(reader.output_list[0]['mr'].output_pixdim,
                          ((2.0, 2.0, 2.0),))
@@ -242,9 +259,9 @@ class Read2D_1D_x1y_Test(tf.test.TestCase):
 
     def test_2D_multimodal_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D_2,
-            'filename_contains': 'x_1_y',
-            'pixdim': (2, 1.5, 2),
-            'axcodes': 'RAS'}}
+                             'filename_contains': 'x_1_y',
+                             'pixdim': (2, 1.5, 2),
+                             'axcodes': 'RAS'}}
         grouping_param = {'ct': ('mr', 'mr', 'mr')}
         reader = ImageReader().initialise(data_param, grouping_param)
         self.assertDictEqual(reader.spatial_ranks, {'ct': 2})
@@ -260,14 +277,15 @@ class Read2D_1D_x1y_Test(tf.test.TestCase):
         self.assertDictEqual(interp, {'ct': (1, 1, 1)})
         self.assertEqual(data['ct'].shape, (100, 100, 1, 1, 3))
 
+
 class Read2D_colorTest(tf.test.TestCase):
     # loading 2d images of rank 3: [x, y, 3] or [x, y, 4]
     def test_no_2d_resampling_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-            'csv_file': '2d_test.csv',
-            'filename_contains': '_u',
-            'pixdim': (2, 2, 2),
-            'axcodes': 'RAS'}}
+                             'csv_file': '2d_test.csv',
+                             'filename_contains': '_u',
+                             'pixdim': (2, 2, 2),
+                             'axcodes': 'RAS'}}
         reader = ImageReader().initialise(data_param)
         self.assertEqual(reader.output_list[0]['mr'].output_pixdim,
                          ((2.0, 2.0, 2.0),))
@@ -282,10 +300,11 @@ class Read2D_colorTest(tf.test.TestCase):
         self.assertEqual(data['mr'].shape, (100, 100, 1, 1, 3))
 
     def test_2D_multimodal_properties(self):
-        data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-            'filename_contains': '_u',
-            'pixdim': (2, 1.5, 2),
-            'axcodes': 'RAS'}}
+        data_param = {
+            'mr': {'path_to_search': IMAGE_PATH_2D,
+                   'filename_contains': '_u',
+                   'pixdim': (2, 1.5, 2),
+                   'axcodes': 'RAS'}}
         grouping_param = {'ct': ('mr', 'mr', 'mr')}
         reader = ImageReader().initialise(data_param, grouping_param)
         self.assertDictEqual(reader.spatial_ranks, {'ct': 2})
@@ -305,10 +324,11 @@ class Read2D_colorTest(tf.test.TestCase):
 class Read3DTest(tf.test.TestCase):
     # loading 3d images of rank 3: [x, y, z]
     def test_3d_resampling_properties(self):
-        data_param = {'mr': {'path_to_search': IMAGE_PATH_3D,
-            'filename_contains': 'Lesion',
-            'pixdim': (4, 3, 4),
-            'axcodes': 'RAS'}}
+        data_param = {
+            'mr': {'path_to_search': IMAGE_PATH_3D,
+                   'filename_contains': 'Lesion',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'}}
         reader = ImageReader().initialise(data_param)
         self.assertDictEqual(reader.spatial_ranks, {'mr': 3})
         self.assertEqual(reader.output_list[0]['mr'].output_pixdim,
@@ -329,14 +349,15 @@ class Read3DTest(tf.test.TestCase):
         """
         loading two modalities, grouping subject names only
         """
-        data_param = {'mr': {'path_to_search': IMAGE_PATH_3D,
-            'filename_contains': 'Lesion',
-            'pixdim': (4, 3, 4),
-            'axcodes': 'RAS'},
+        data_param = {
+            'mr': {'path_to_search': IMAGE_PATH_3D,
+                   'filename_contains': 'Lesion',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'},
             'ct': {'path_to_search': IMAGE_PATH_3D,
-                'filename_contains': 'Lesion',
-                'pixdim': (4, 3, 4),
-                'axcodes': 'RAS'}}
+                   'filename_contains': 'Lesion',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'}}
         reader = ImageReader().initialise(data_param)
         self.assertDictEqual(reader.spatial_ranks, {'mr': 3, 'ct': 3})
         self.assertEqual(reader.output_list[0]['mr'].output_pixdim,
@@ -359,14 +380,15 @@ class Read3DTest(tf.test.TestCase):
         """
         loading two modalities, grouping subject names only
         """
-        data_param = {'mr': {'path_to_search': IMAGE_PATH_3D,
-            'filename_contains': 'LesionFin',
-            'pixdim': (4, 3, 4),
-            'axcodes': 'RAS'},
+        data_param = {
+            'mr': {'path_to_search': IMAGE_PATH_3D,
+                   'filename_contains': 'LesionFin',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'},
             'ct': {'path_to_search': IMAGE_PATH_3D,
-                'filename_contains': 'FLAIR',
-                'pixdim': (4, 3, 4),
-                'axcodes': 'RAS'}}
+                   'filename_contains': 'FLAIR',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'}}
         grouping_param = {'image': ('mr', 'ct')}
         reader = ImageReader().initialise(data_param, grouping_param)
         self.assertDictEqual(reader.spatial_ranks, {'image': 3})
@@ -382,6 +404,92 @@ class Read3DTest(tf.test.TestCase):
         self.assertDictEqual(interp, {'image': (1, 1)})
         # allows rounding error spatially
         self.assertAllClose(data['image'].shape[:3], (62, 83, 62), atol=1)
+        self.assertAllClose(data['image'].shape[3:], (1, 2))
+
+
+class Read3D_1_1_Test(tf.test.TestCase):
+    # loading 5d images of rank 3: [x, y, z, 1, 1]
+    def test_3d_resampling_properties(self):
+        data_param = {
+            'mr': {'path_to_search': IMAGE_PATH_3D_1,
+                   'filename_contains': 'x_y_z_1_1',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'}}
+        reader = ImageReader().initialise(data_param)
+        self.assertDictEqual(reader.spatial_ranks, {'mr': 3})
+        self.assertEqual(reader.output_list[0]['mr'].output_pixdim,
+                         ((4.0, 3.0, 4.0),))
+        self.assertEqual(reader.output_list[0]['mr'].output_axcodes,
+                         (('R', 'A', 'S'),))
+        idx, data, interp = reader()
+
+        # test output
+        self.assertEqual(list(data), ['mr'])
+        self.assertTrue(idx in range(len(reader.output_list)))
+        self.assertDictEqual(interp, {'mr': (1,)})
+        # allows rounding error spatially
+        self.assertAllClose(data['mr'].shape[:3], (12, 8, 10), atol=1)
+        self.assertAllClose(data['mr'].shape[3:], (1, 1))
+
+    def test_3d_multiple_properties(self):
+        """
+        loading two modalities, grouping subject names only
+        """
+        data_param = {
+            'mr': {'path_to_search': IMAGE_PATH_3D_1,
+                   'filename_contains': 'x_y_z_1_1',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'},
+            'ct': {'path_to_search': IMAGE_PATH_3D_1,
+                   'filename_contains': 'x_y_z_1_1',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'}}
+        reader = ImageReader().initialise(data_param)
+        self.assertDictEqual(reader.spatial_ranks, {'mr': 3, 'ct': 3})
+        self.assertEqual(reader.output_list[0]['mr'].output_pixdim,
+                         ((4.0, 3.0, 4.0),))
+        self.assertEqual(reader.output_list[0]['mr'].output_axcodes,
+                         (('R', 'A', 'S'),))
+        idx, data, interp = reader()
+
+        # test output
+        self.assertEqual(list(data), ['mr', 'ct'])
+        self.assertTrue(idx in range(len(reader.output_list)))
+        self.assertDictEqual(interp, {'mr': (1,), 'ct': (1,)})
+        # allows rounding error spatially
+        self.assertAllClose(data['mr'].shape[:3], (12, 8, 10), atol=1)
+        self.assertAllClose(data['mr'].shape[3:], (1, 1))
+        self.assertAllClose(data['ct'].shape[:3], (12, 8, 10), atol=1)
+        self.assertAllClose(data['ct'].shape[3:], (1, 1))
+
+    def test_3d_concat_properties(self):
+        """
+        loading two modalities, grouping subject names only
+        """
+        data_param = {
+            'mr': {'path_to_search': IMAGE_PATH_3D_1,
+                   'filename_contains': 'x_y_z_1_1',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'},
+            'ct': {'path_to_search': IMAGE_PATH_3D_1,
+                   'filename_contains': 'x_y_z_1_1',
+                   'pixdim': (4, 3, 4),
+                   'axcodes': 'RAS'}}
+        grouping_param = {'image': ('mr', 'ct')}
+        reader = ImageReader().initialise(data_param, grouping_param)
+        self.assertDictEqual(reader.spatial_ranks, {'image': 3})
+        self.assertEqual(reader.output_list[0]['image'].output_pixdim,
+                         ((4.0, 3.0, 4.0),) * 2)
+        self.assertEqual(reader.output_list[0]['image'].output_axcodes,
+                         (('R', 'A', 'S'),) * 2)
+        idx, data, interp = reader()
+
+        # test output
+        self.assertEqual(list(data), ['image'])
+        self.assertTrue(idx in range(len(reader.output_list)))
+        self.assertDictEqual(interp, {'image': (1, 1)})
+        # allows rounding error spatially
+        self.assertAllClose(data['image'].shape[:3], (12, 8, 10), atol=1)
         self.assertAllClose(data['image'].shape[3:], (1, 2))
 
 
