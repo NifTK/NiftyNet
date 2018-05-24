06ecb698b7b780cc22df2604503729ecfc733cbd
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-05-24 06:06:13 +0100

    Issue #212: set tag prefix to test tag prefix

--- niftynet/utilities/versioneer_version.py
+++ niftynet/utilities/versioneer_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "None"
+    cfg.tag_prefix = "issue212TestTag"
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "niftynet/utilities/versioneer_version.py"
     cfg.verbose = False
