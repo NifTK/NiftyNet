cb8c3c7633dfb555d64f52b78e141564054164a6
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-06-04 10:52:29 +0100

    Issue #105: removed deactivation of redefined-variable-type pylint error, which is an extension (unavailable by default) as of pylint version 1.7: https://github.com/PyCQA/pylint/blob/master/doc/whatsnew/1.7.rst

--- niftynet/engine/image_window_buffer.py
+++ niftynet/engine/image_window_buffer.py
@@ -102,7 +102,6 @@ class InputBatchQueueRunner(object):
             if not is_dynamic_window else None
 
         # create a queue
-        # pylint: disable=redefined-variable-type
         if self.shuffle:
             self._queue = tf.RandomShuffleQueue(
                 capacity=self.capacity,
