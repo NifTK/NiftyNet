804255d50fc40a0e4d39ba39a965fe6fd57ef0e0
Fernando Perez-Garcia <fepegar@gmail.com>
2018-08-10 00:43:20 +0100

    Fix variable name

--- tests/loss_segmentation_test.py
+++ tests/loss_segmentation_test.py
@@ -262,11 +262,12 @@ class TverskyTest(tf.test.TestCase):
                 [[0, 10], [10, 0], [10, 0], [10, 0]],
                 dtype=tf.float32, name='predicted')
             labels = tf.constant([1, 0, 0, 0], dtype=tf.int64, name='labels')
-            predicted, labels = [tf.expand_dims(x, axis=0) for x in (predicted, labels)]
+            predicted, labels = [tf.expand_dims(x, axis=0)
+                                 for x in (predicted, labels)]
 
             test_loss_func = LossFunction(2, loss_type='Tversky')
-            one_minus_dice_score = test_loss_func(predicted, labels)
-            self.assertAllClose(one_minus_dice_score.eval(), 0.0, atol=1e-4)
+            one_minus_tversky_index = test_loss_func(predicted, labels)
+            self.assertAllClose(one_minus_tversky_index.eval(), 0.0, atol=1e-4)
 
     def test_tversky_index_weights(self):
         with self.test_session():
@@ -276,13 +277,14 @@ class TverskyTest(tf.test.TestCase):
                 [[0, 10], [10, 0], [10, 0], [10, 0]],
                 dtype=tf.float32, name='predicted')
             labels = tf.constant([[1, 0, 1, 0]], dtype=tf.int64, name='labels')
-            predicted, labels = [tf.expand_dims(x, axis=0) for x in (predicted, labels)]
+            predicted, labels = [tf.expand_dims(x, axis=0)
+                                 for x in (predicted, labels)]
 
             test_loss_func = LossFunction(2,
                                           loss_type='Tversky')
-            one_minus_dice_score = test_loss_func(predicted, labels,
-                                                  weight_map=weights)
-            self.assertAllClose(one_minus_dice_score.eval(), 0.0, atol=1e-4)
+            one_minus_tversky_index = test_loss_func(predicted, labels,
+                                                     weight_map=weights)
+            self.assertAllClose(one_minus_tversky_index.eval(), 0.0, atol=1e-4)
 
     def test_wrong_prediction(self):
         with self.test_session():
@@ -290,11 +292,12 @@ class TverskyTest(tf.test.TestCase):
                 [[0, 100]],
                 dtype=tf.float32, name='predicted')
             labels = tf.constant([0], dtype=tf.int64, name='labels')
-            predicted, labels = [tf.expand_dims(x, axis=0) for x in (predicted, labels)]
+            predicted, labels = [tf.expand_dims(x, axis=0)
+                                 for x in (predicted, labels)]
 
             test_loss_func = LossFunction(2, loss_type='Tversky')
-            one_minus_dice_score = test_loss_func(predicted, labels)
-            self.assertAlmostEqual(one_minus_dice_score.eval(), 1.0)
+            one_minus_tversky_index = test_loss_func(predicted, labels)
+            self.assertAlmostEqual(one_minus_tversky_index.eval(), 1.0)
 
 
 class DiceDenseTest(tf.test.TestCase):
