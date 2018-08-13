2b651e51a0ff1c72aad7052c8249cac3681bb7e4
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-08-13 10:20:54 +0100

    Issue #168: thinly wrapped all methods of NiftyNetLaunchConfig inherited from ConfigParser

--- niftynet/utilities/niftynet_launch_config.py
+++ niftynet/utilities/niftynet_launch_config.py
@@ -11,6 +11,33 @@ except ImportError:
 
 
 class NiftyNetLaunchConfig(configparser.ConfigParser):
-    """Launch configuration settings"""
+    """
+    Launch configuration settings.
 
-    pass
+    This class provides the same interface and functionality
+    as the built-in `ConfigParser` class. Beyond that, it
+    can also parse the new YAML configuration format of
+    NiftyNet. The former is for backwards compatibility only,
+    while the latter is the purpose of this class' life.
+    """
+
+    def read(self, filenames, encoding=None):
+        return super(NiftyNetLaunchConfig, self).read(filenames, encoding)
+
+    def sections(self):
+        return super(NiftyNetLaunchConfig, self).sections()
+
+    def items(self, section=configparser._UNSET, raw=False, vars=None):
+        return super(NiftyNetLaunchConfig, self).items(section, raw, vars)
+
+    def add_section(self, section):
+        super(NiftyNetLaunchConfig, self).add_section(section)
+
+    def set(self, section, option, value=None):
+        super(NiftyNetLaunchConfig, self).set(section, option, value)
+
+    def remove_section(self, section):
+        super(NiftyNetLaunchConfig, self).remove_section(section)
+
+    def has_section(self, section):
+        return super(NiftyNetLaunchConfig, self).has_section(section)
