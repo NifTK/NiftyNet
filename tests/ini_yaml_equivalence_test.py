924cd71663a4c10ada4f41a6f9fce1c023ba70fd
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-08-17 11:50:22 +0100

    Issue #168: added stub for INI-YAML config file equivalence

---
+++ tests/ini_yaml_equivalence_test.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+
+
+class IniYamlEquivalenceTest(TestCase):
+
+    def setUp(self):
+        # TODO
+        raise NotImplementedError
+
+    def test_read_ini_equivalent_to_read_yaml(self):
+        # TODO
+        raise NotImplementedError
+
+    def test_incompatible_file_not_read(self):
+        # TODO
+        raise NotImplementedError
+
+    def test_sections_same(self):
+        # TODO
+        raise NotImplementedError
+
+    def test_items_same(self):
+        # TODO
+        raise NotImplementedError
+
+    def test_add_section_same(self):
+        # TODO
+        raise NotImplementedError
+
+    def test_set_same(self):
+        # TODO
+        raise NotImplementedError
+
+    def test_remove_section_same(self):
+        # TODO
+        raise NotImplementedError
+
+    def has_section_same(self):
+        # TODO
+        raise NotImplementedError
