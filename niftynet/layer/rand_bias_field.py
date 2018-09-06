4746463dd8f8da004988d6fa03c2f738b5d5b54a
Carole Sudre <carole.sudre@kcl.ac.uk>
2018-09-06 14:19:20 +0100

    remove nib saving from final layer

--- niftynet/layer/rand_bias_field.py
+++ niftynet/layer/rand_bias_field.py
@@ -2,8 +2,6 @@
 from __future__ import absolute_import, print_function
 
 import numpy as np
-import scipy.ndimage
-import nibabel as nib
 
 from niftynet.layer.base_layer import RandomisedLayer
 
@@ -70,7 +68,7 @@ class RandomBiasFieldLayer(RandomisedLayer):
         if spatial_rank == 3:
             z_range = np.arange(-shape[2] / 2, shape[2] / 2)
             x_mesh, y_mesh, z_mesh = np.asarray(np.meshgrid(x_range, y_range,
-                                                       z_range), dtype=float)
+                                                z_range), dtype=float)
             x_mesh /= float(np.max(x_mesh))
             y_mesh /= float(np.max(y_mesh))
             z_mesh /= float(np.max(z_mesh))
@@ -80,8 +78,8 @@ class RandomBiasFieldLayer(RandomisedLayer):
                         rand_coeff = self._bf_coeffs[i]
 
                         new_map = rand_coeff * np.power(x_mesh, order_x) * \
-                                            np.power(y_mesh, order_y) * \
-                                  np.power(z_mesh, order_z)
+                            np.power(y_mesh, order_y) * \
+                            np.power(z_mesh, order_z)
                         # print(np.asarray(np.where(np.abs(new_map) >
                         #                           0)).shape, np.unique(
                         #     new_map).shape)
@@ -96,7 +94,7 @@ class RandomBiasFieldLayer(RandomisedLayer):
                 for order_y in range(0, self.order+1 - order_x):
                     rand_coeff = self._bf_coeffs[i]
                     new_map = rand_coeff * np.power(x_mesh, order_x) * \
-                                            np.power(y_mesh, order_y)
+                        np.power(y_mesh, order_y)
                     bf_map += np.transpose(new_map, (1, 0))
                     i += 1
         return np.exp(bf_map)
@@ -112,16 +110,7 @@ class RandomBiasFieldLayer(RandomisedLayer):
         '''
         assert self._bf_coeffs is not None
         bf_map = self._generate_bias_field_map(image.shape)
-        print(np.asarray(np.where(np.abs(bf_map)>0)).shape)
         bf_image = image * bf_map
-        print(bf_image.shape, image.shape, np.max(bf_image), np.max(image),
-              np.max(bf_map), np.min(bf_map))
-        bf_nii = nib.Nifti1Image(bf_map, np.diag([1,1,1,1]))
-        bf_image_nii = nib.Nifti1Image(bf_image, np.diag([1,1,1,1]))
-        image_nii = nib.Nifti1Image(image, np.diag([1,1,1,1]))
-        nib.save(bf_nii, './TestBF.nii.gz')
-        nib.save(bf_image_nii, './TestModif.nii.gz')
-        nib.save(image_nii, './InitModify.nii.gz')
         return bf_image
 
     def layer_op(self, inputs, interp_orders, *args, **kwargs):
@@ -144,25 +133,4 @@ class RandomBiasFieldLayer(RandomisedLayer):
                         raise NotImplementedError("unknown input format")
         return inputs
 
-        # if inputs.spatial_rank == 3:
-        #    if inputs.data.ndim == 4:
-        #        for mod_i in range(inputs.data.shape[-1]):
-        #            inputs.data[..., mod_i] = self._apply_transformation_3d(
-        #                inputs.data[..., mod_i], inputs.interp_order)
-        #    if inputs.data.ndim == 5:
-        #        for t in range(inputs.data.shape[-1]):
-        #            for mod_i in range(inputs.data.shape[-2]):
-        #                inputs.data[..., mod_i, t] = \
-        #                    self._apply_transformation_3d(
-        #                      inputs.data[..., mod_i, t], inputs.interp_order)
-        #    if inputs.interp_order > 0:
-        #        inputs.data = inputs.data.astype(np.float)
-        #    elif inputs.interp_order == 0:
-        #        inputs.data = inputs.data.astype(np.int64)
-        #    else:
-        #        raise ValueError('negative interpolation order')
-        #    return inputs
-        # else:
-        #    # TODO: rotation for spatial_rank is 2
-        #    # currently not supported 2/2.5D rand rotation
-        #    return inputs
+
