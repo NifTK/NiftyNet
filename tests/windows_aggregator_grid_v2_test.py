bf6a917b07f5a2fdb6f8d9ecd63e68f1ce031839
Wenqi Li <wenqi.li@ucl.ac.uk>
2018-09-20 17:29:10 +0100

    Update windows_aggregator_grid_v2_test.py

--- tests/windows_aggregator_grid_v2_test.py
+++ tests/windows_aggregator_grid_v2_test.py
@@ -213,7 +213,7 @@ class GridSamplesAggregatorTest(tf.test.TestCase):
                                    'aggregated',
                                    output_filename)
         self.assertAllClose(
-            nib.load(output_file).shape, [168, 256, 256, 1, 1],
+            nib.load(output_file).shape, [256, 168, 256, 1, 1],
             rtol=1e-03, atol=1e-03)
         sampler.close_all()
 
