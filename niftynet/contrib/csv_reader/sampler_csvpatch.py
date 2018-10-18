8d98652994aef112ad499eef55456f52254e1067
Carole Sudre <carole.sudre@kcl.ac.uk>
2018-10-18 17:31:44 +0100

    Initial CSV Patch WIP

--- niftynet/contrib/csv_reader/sampler_csvpatch.py
+++ niftynet/contrib/csv_reader/sampler_csvpatch.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Generating image window by weighted sampling map from input image
-This can also be considered as a "weighted random cropping" layer of the
+Generating uniformly distributed image window from input image
+This can also be considered as a "random cropping" layer of the
 input image.
 """
 from __future__ import absolute_import, division, print_function
@@ -9,140 +9,313 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import tensorflow as tf
 
-from niftynet.engine.sampler_uniform_v2 import UniformSampler
-from niftynet.engine.image_window import N_SPATIAL
+
+from niftynet.contrib.csv_reader.sampler_csv_rows import ImageWindowDatasetCSV
+from niftynet.engine.image_window import LOCATION_FORMAT
+from niftynet.engine.image_window_dataset import ImageWindowDataset
+from niftynet.engine.image_window import N_SPATIAL, LOCATION_FORMAT
 
 
-class WeightedSampler(UniformSampler):
+class CSVPatchSampler(ImageWindowDatasetCSV):
     """
-    This class generators samples from a user provided
-    frequency map for each input volume
-    The sampling likelihood of each voxel (and window around)
-    is proportional to its frequency
+    This class generates samples by uniformly sampling each input volume
+    currently the coordinates are randomised for spatial dims only,
+    i.e., the first three dims of image.
 
-    This is implemented in a closed form using cumulative histograms
-    for efficiency purposes i.e., the first three dims of image.
-
-    This layer can be considered as a "weighted random cropping" layer of the
+    This layer can be considered as a "random cropping" layer of the
     input image.
     """
 
     def __init__(self,
-                 reader,
+                 reader, csv_reader,
                  window_sizes,
                  batch_size=1,
                  windows_per_image=1,
                  queue_length=10,
-                 name='weighted_sampler'):
-        UniformSampler.__init__(self,
-                                reader=reader,
-                                window_sizes=window_sizes,
-                                batch_size=batch_size,
-                                windows_per_image=windows_per_image,
-                                queue_length=queue_length,
-                                name=name)
-        tf.logging.info('Initialised weighted sampler window instance')
-        self.window_centers_sampler = weighted_spatial_coordinates
+                 name='uniform_sampler_v2'):
+        ImageWindowDatasetCSV.__init__(
+            self,
+            reader=reader,
+            csv_reader=csv_reader,
+            window_sizes=window_sizes,
+            batch_size=batch_size,
+            windows_per_image=windows_per_image,
+            queue_length=queue_length,
+            shuffle=True,
+            epoch=-1,
+            smaller_final_batch_mode='drop',
+            name=name)
+
+        tf.logging.info("initialised uniform sampler %s ", self.window.shapes)
+        self.window_centers_sampler = csvcenter_spatial_coordinates
+
+    # pylint: disable=too-many-locals
+    def layer_op(self, idx=None):
+        """
+        This function generates sampling windows to the input buffer
+        image data are from ``self.reader()``
+
+        It first completes window shapes based on image data,
+        then finds random coordinates based on the window shapes
+        finally extract window with the coordinates and output
+        a dictionary (required by input buffer).
+
+        :return: output data dictionary
+            ``{image_modality: data_array, image_location: n_samples * 7}``
+        """
+        csv_sampler = None
+        for csv_reader_inst in self.csv_reader:
+            if csv_reader_inst.task_param == "sampler":
+                csv_sampler = csv_reader_inst
+        flag_multi_row = test_imbalance_rows(csv_sampler)
+        if flag_multi_row:
+            _, _, idx = csv_sampler.reader(shuffle=True)
+
+        image_id, data, _ = self.reader(idx=idx, shuffle=True)
+        subj_indices, csv_data, _ = self.csv_reader(subject_id=idx,
+                                                        shuffle=True)
+
+        image_shapes = dict(
+            (name, data[name].shape) for name in self.window.names)
+        static_window_shapes = self.window.match_image_shapes(image_shapes)
+
+        # find random coordinates based on window and image shapes
+        coordinates, idx, pb_coord = self._spatial_coordinates_generator(
+            subject_id=image_id,
+            data=data,
+            csv_sampler=csv_sampler,
+            img_sizes=image_shapes,
+            win_sizes=static_window_shapes,
+            n_samples=self.window.n_samples
+            )
+        coordinates_corr = correction_coordinates(coordinates, idx, pb_coord,
+                                                  csv_sampler, img_sizes,
+                                                  win_sizes)
+
+        # initialise output dict, placeholders as dictionary keys
+        # this dictionary will be used in
+        # enqueue operation in the form of: `feed_dict=output_dict`
+        output_dict = {}
+        # fill output dict with data
+        for name in list(data):
+            coordinates_key = LOCATION_FORMAT.format(name)
+            image_data_key = name
+
+            # fill the coordinates
+            location_array = coordinates[name]
+            output_dict[coordinates_key] = location_array
+
+            # fill output window array
+            image_array = []
+            for window_id in range(self.window.n_samples):
+                x_start, y_start, z_start, x_end, y_end, z_end = \
+                    location_array[window_id, 1:]
+                try:
+                    image_window = data[name][
+                        x_start:x_end, y_start:y_end, z_start:z_end, ...]
+                    image_array.append(image_window[np.newaxis, ...])
+                except ValueError:
+                    tf.logging.fatal(
+                        "dimensionality miss match in input volumes, "
+                        "please specify spatial_window_size with a "
+                        "3D tuple and make sure each element is "
+                        "smaller than the image length in each dim. "
+                        "Current coords %s", location_array[window_id])
+                    raise
+            if len(image_array) > 1:
+                output_dict[image_data_key] = \
+                    np.concatenate(image_array, axis=0)
+            else:
+                output_dict[image_data_key] = image_array[0]
+        # the output image shape should be
+        # [enqueue_batch_size, x, y, z, time, modality]
+        # where enqueue_batch_size = windows_per_image
+        return output_dict
+
+    def _spatial_coordinates_generator(self,
+                                       subject_id,
+                                       data,
+                                       csv_sampler,
+                                       img_sizes,
+                                       win_sizes,
+                                       mode='pad',
+                                       n_samples=1):
+        """
+        Generate spatial coordinates for sampling.
+
+        Values in ``win_sizes`` could be different --
+        for example in a segmentation network ``win_sizes`` could be
+        ``{'training_image_spatial_window': (32, 32, 10),
+           'Manual_label_spatial_window': (16, 16, 10)}``
+        (the network reduces x-y plane spatial resolution).
+
+        This function handles this situation by first find the largest
+        window across these window definitions, and generate the coordinates.
+        These coordinates are then adjusted for each of the
+        smaller window sizes (the output windows are almost concentric).
+        """
+
+        assert data is not None, "No input from image reader. Please check" \
+                                 "the configuration file."
+
+        # infer the largest spatial window size and check image spatial shapes
+        img_spatial_size, win_spatial_size = \
+            _infer_spatial_size(img_sizes, win_sizes)
+
+        window_centres = None
+        # try:
+        #     window_centres = csv_data.get('sampler', None)
+        # except AttributeError:
+        #     pass
+
+        n_samples = max(n_samples, 1)
+        if csv_sampler is None:
+            window_centres = rand_spatial_coordinates(n_samples,
+                                                            img_spatial_size,
+                                                            win_spatial_size,
+                                                            None)
+            list_idx = np.arange(0, n_samples)
+        else :
+            window_centres_list = []
+            list_idx = []
+            for n in range(0, n_samples):
+                idx, data_csv, _ = csv_sampler.csv_reader(
+                    subject_id=subject_id, shuffle=True)
+                centre_tmp = np.squeeze(data_csv['sampler'])
+                list_idx.append(idx)
+                window_centres_list.append(centre_tmp)
+                window_centres = np.concatenate(window_centres_list, 0)
+
+        assert window_centres.shape == (n_samples, N_SPATIAL), \
+            "the coordinates generator should return " \
+            "{} samples of rank {} locations".format(n_samples, N_SPATIAL)
+
+        # adjust spatial coordinates based on each mod spatial window size
+        all_coordinates = {}
+        pb_coordinates = {}
+        for mod in list(win_sizes):
+            win_size = np.asarray(win_sizes[mod][:N_SPATIAL])
+            half_win = np.floor(win_size / 2.0).astype(int)
+
+            # Make starting coordinates of the window
+            spatial_coords = np.zeros(
+                (1, N_SPATIAL * 2), dtype=np.int32)
+            spatial_coords[:, :N_SPATIAL] = np.maximum(
+                window_centres[0, :N_SPATIAL] - half_win[:N_SPATIAL], 0)
+            sign_pb_min = np.square(np.sign(window_centres[0,
+                                          :N_SPATIAL]-half_win[
+                                                             :N_SPATIAL])) - \
+                      np.sign(window_centres[0, :N_SPATIAL] - half_win[
+                                                              :N_SPATIAL])
+            sign_pb_min /= 2
+
+            # Make the opposite corner of the window is
+            # just adding the mod specific window size
+            spatial_coords[:, N_SPATIAL:] = \
+                spatial_coords[:, :N_SPATIAL] + win_size[:N_SPATIAL]
+            sign_pb_max = np.sign(spatial_coords[:, N_SPATIAL:] - img_spatial_size)
+            pb_coordinates_tmp = np.where(sign_pb_max == 1, np.ones_like(
+                sign_pb_max), np.zeros_like(sign_pb_max))
+            pb_coordinates_tmp += sign_pb_min
+
+            assert np.all(spatial_coords[:, N_SPATIAL:] <= img_spatial_size), \
+                'spatial coords: out of bounds.'
+
+            # include subject id as the 1st column of all_coordinates values
+            subject_id = np.ones((n_samples,), dtype=np.int32) * subject_id
+            spatial_coords = np.append(
+                subject_id[:, None], spatial_coords, axis=1)
+            all_coordinates[mod] = spatial_coords
+            pb_coordinates[mod] = pb_coordinates_tmp
 
 
-def weighted_spatial_coordinates(
+        return all_coordinates, list_idx, pb_coordinates
+
+
+def correction_coordinates(coordinates, idx, pb_coord, img_sizes, win_sizes,
+                           csv_sampler, mode="remove"):
+    # infer the largest spatial window size and check image spatial shapes
+    img_spatial_size, win_spatial_size = \
+        _infer_spatial_size(img_sizes, win_sizes)
+    overall_pb = np.zeros([len(idx), 1])
+    for mod in list(win_sizes):
+        overall_pb += np.sum(pb_coord[mod], 1)
+    if np.sum(overall_pb) == 0:
+        return coordinates, None
+    else:
+        if mode == "remove":
+            list_nopb = np.where(overall_pb == 0)
+            list_pb = np.where(overall_pb > 0)
+            for mod in list(win_sizes):
+                coordinates[mod]=coordinates[mod][list_nopb, :]
+            csv_sampler._df = csv_sampler._df.drop(list_pb)
+            return coordinates, None
+        elif mode == "pad" :
+            return coordinates, padding_corr
+        else:
+            return shifted_coordinates, None
+
+
+
+
+
+def rand_spatial_coordinates(
         n_samples, img_spatial_size, win_spatial_size, sampler_map):
     """
-    Weighted sampling from a map.
-    This function uses a cumulative histogram for fast sampling.
-
-    see also `sampler_uniform.rand_spatial_coordinates`
+    Generate spatial coordinates from a discrete uniform distribution.
 
     :param n_samples: number of random coordinates to generate
     :param img_spatial_size: input image size
     :param win_spatial_size: input window size
-    :param sampler_map: sampling prior map, it's spatial shape should be
-            consistent with `img_spatial_size`
+    :param sampler_map: sampling prior map (not in use)
     :return: (n_samples, N_SPATIAL) coordinates representing sampling
               window centres relative to img_spatial_size
     """
-    assert sampler_map is not None, \
-        'sampling prior map is not specified, ' \
-        'please check `sampler=` option in the config.'
-    # Get the cumulative sum of the normalised sorted intensities
-    # i.e. first sort the sampling frequencies, normalise them
-    # to sum to one, and then accumulate them in order
-    assert np.all(img_spatial_size[:N_SPATIAL] ==
-                  sampler_map.shape[:N_SPATIAL]), \
-        'image and sampling map shapes do not match'
-    win_spatial_size = np.asarray(win_spatial_size, dtype=np.int32)
-    cropped_map = crop_sampling_map(sampler_map, win_spatial_size)
-    flatten_map = cropped_map.flatten()
-    flatten_map = flatten_map - np.min(flatten_map)
-    normaliser = flatten_map.sum()
-    # get the sorting indexes to that we can invert the sorting later on.
-    sorted_indexes = np.argsort(flatten_map)
-    sorted_data = np.cumsum(
-        np.true_divide(flatten_map[sorted_indexes], normaliser))
+    tf.logging.debug('uniform sampler, prior %s ignored', sampler_map)
 
-    middle_coords = np.zeros((n_samples, N_SPATIAL), dtype=np.int32)
-    for sample in range(0, n_samples):
-        # get n_sample from the cumulative histogram, spaced by 1/n_samples,
-        # plus a random perturbation to give us a stochastic sampler
-        sample_ratio = 1 - (np.random.random() + sample) / (n_samples + 1)
-        # find the index where the cumulative it above the sample threshold
-        try:
-            if normaliser == 0:
-                # constant map? reducing to a uniform sampling
-                sample_index = np.random.randint(len(sorted_data))
-            else:
-                sample_index = np.argmax(sorted_data >= sample_ratio)
-        except ValueError:
-            tf.logging.fatal("unable to choose sampling window based on "
-                             "the current frequency map.")
-            raise
-        # invert the sample index to the pre-sorted index
-        inverted_sample_index = sorted_indexes[sample_index]
-        # get the x,y,z coordinates on the cropped_map
-        middle_coords[sample, :N_SPATIAL] = np.unravel_index(
-            inverted_sample_index, cropped_map.shape)[:N_SPATIAL]
-
-    # re-shift coords due to the crop
-    half_win = np.floor(win_spatial_size / 2).astype(np.int32)
-    middle_coords[:, :N_SPATIAL] = \
-        middle_coords[:, :N_SPATIAL] + half_win[:N_SPATIAL]
-    return middle_coords
+    # Sample coordinates at random
+    half_win = np.floor(np.asarray(win_spatial_size) / 2.0).astype(np.int32)
+    max_coords = np.zeros((n_samples, N_SPATIAL), dtype=np.int32)
+    for (idx, (img, win)) in enumerate(
+            zip(img_spatial_size[:N_SPATIAL], win_spatial_size[:N_SPATIAL])):
+        max_coords[:, idx] = np.random.randint(
+            0, max(img - win + 1, 1), n_samples)
+    max_coords[:, :N_SPATIAL] = \
+        max_coords[:, :N_SPATIAL] + half_win[:N_SPATIAL]
+    return max_coords
 
 
-def crop_sampling_map(input_map, win_spatial_size):
+def _infer_spatial_size(img_sizes, win_sizes):
     """
-    Utility function for generating a cropped version of the
-    input sampling prior map (the input weight map where the centre of
-    the window might be). If the centre of the window was outside of
-    this crop area, the patch would be outside of the field of view
+    Utility function to find the spatial size of image,
+    and the largest spatial window size across input sections.
 
-    :param input_map: the input weight map where the centre of
-                      the window might be
-    :param win_spatial_size: size of the borders to be cropped
-    :return: cropped sampling map
+    Raises NotImplementedError if the images have
+    different spatial dimensions.
+
+    :param img_sizes: dictionary of {'input_name': (img_size_x, img_size,y,...)}
+    :param win_sizes: dictionary of {'input_name': (win_size_x, win_size_y,...)}
+    :return: (image_spatial_size, window_largest_spatial_size)
     """
+    uniq_spatial_size = \
+        set([img_size[:N_SPATIAL] for img_size in list(img_sizes.values())])
+    if len(uniq_spatial_size) != 1:
+        tf.logging.fatal("Don't know how to generate sampling "
+                         "locations: Spatial dimensions of the "
+                         "grouped input sources are not "
+                         "consistent. %s", uniq_spatial_size)
+        raise NotImplementedError
+    img_spatial_size = np.asarray(uniq_spatial_size.pop(), dtype=np.int32)
 
-    # prepare cropping indices
-    _start, _end = [], []
-    for win_size, img_size in \
-            zip(win_spatial_size[:N_SPATIAL], input_map.shape[:N_SPATIAL]):
-        # cropping floor of the half window
-        d_start = int(win_size / 2.0)
-        # using ceil of half window
-        d_end = img_size - win_size + int(win_size / 2.0 + 0.6)
+    # find the largest spatial window across input sections
+    _win_spatial_sizes = \
+        [win_size[:N_SPATIAL] for win_size in win_sizes.values()]
+    _win_spatial_sizes = np.asarray(_win_spatial_sizes, dtype=np.int32)
+    win_spatial_size = np.max(_win_spatial_sizes, axis=0)
 
-        _start.append(d_start)
-        _end.append(d_end + 1 if d_start == d_end else d_end)
+    assert all([img_spatial_size[i] >= win_spatial_size[i]
+                for i in range(N_SPATIAL)]), \
+        "window size {} is larger than image size {}".format(
+            win_spatial_size, img_spatial_size)
 
-    try:
-        assert len(_start) == 3
-        cropped_map = input_map[
-            _start[0]:_end[0], _start[1]:_end[1], _start[2]:_end[2], 0, 0]
-        assert np.all(cropped_map.shape) > 0
-    except (IndexError, KeyError, TypeError, AssertionError):
-        tf.logging.fatal(
-            "incompatible map: %s and window size: %s\n"
-            "try smaller (fully-specified) spatial window sizes?",
-            input_map.shape, win_spatial_size)
-        raise
-    return cropped_map
+    return img_spatial_size, win_spatial_size
