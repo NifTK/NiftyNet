2473168f259536b11d0042d59d887a849d07392e
Thomas <ucabtmv@ucl.ac.uk>
2018-10-26 15:55:41 +0000

    changed csv_file -> csv_path_file in various places and merged carole changes

--- tests/reader_modular_test.py
+++ tests/reader_modular_test.py
@@ -139,7 +139,7 @@ class Read2DTest(NiftyNetTestCase):
 
     def test_input_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-                             'csv_file': '2d_test.csv'}}
+                             'csv_path_file': '2d_test.csv'}}
         reader = ImageReader().initialise(data_param)
         self.default_property_asserts(reader)
         idx, data, interp = reader()
@@ -152,7 +152,7 @@ class Read2DTest(NiftyNetTestCase):
 
     def test_no_2d_resampling_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-                             'csv_file': '2d_test.csv',
+                             'csv_path_file': '2d_test.csv',
                              'pixdim': (2, 2, 2),
                              'axcodes': 'RAS'}}
         reader = ImageReader().initialise(data_param)
@@ -200,7 +200,7 @@ class Read2D_1DTest(NiftyNetTestCase):
     # loading 2d images of rank 3: [x, y, 1]
     def test_no_2d_resampling_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D_1,
-                             'csv_file': '2d_test.csv',
+                             'csv_path_file': '2d_test.csv',
                              'filename_contains': '_img',
                              'pixdim': (2, 2, 2),
                              'axcodes': 'RAS'}}
@@ -283,7 +283,7 @@ class Read2D_colorTest(NiftyNetTestCase):
     # loading 2d images of rank 3: [x, y, 3] or [x, y, 4]
     def test_no_2d_resampling_properties(self):
         data_param = {'mr': {'path_to_search': IMAGE_PATH_2D,
-                             'csv_file': '2d_test.csv',
+                             'csv_path_file': '2d_test.csv',
                              'filename_contains': '_u',
                              'pixdim': (2, 2, 2),
                              'axcodes': 'RAS'}}
