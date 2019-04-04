e262fef61c46825a865542137497899ee70cf2b5
Stian Johnsen <stian.johnsen@kcl.ac.uk>
2019-04-04 11:22:36 +0100

    Moved implementations out of header file

--- niftynet/contrib/niftyreg_image_resampling/niftyreg_gpu_resampler/resampler_boundary.h
+++ niftynet/contrib/niftyreg_image_resampling/niftyreg_gpu_resampler/resampler_boundary.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include <cstdlib>
-#include <limits>
-#include <type_traits>
+#ifdef __CUDACC__
+#define NR_HOST_DEV __host__ __device__
+#else
+#define NR_HOST_DEV
+#endif
 
 /** \brief Resampler boundary treatment options */
 enum class resampler_boundary_e {
@@ -13,52 +15,24 @@ enum class resampler_boundary_e {
   SENTINEL, /**< Sentinel code */
 };
 
-/* *************************************************************** */
-#ifdef __CUDACC__
-#define NR_HOST_DEV __host__ __device__
-#else
-#define NR_HOST_DEV
-#endif
-/* *************************************************************** */
 /**
  * \brief Boundary index modification function
  * \tparam tBoundary boundary treatment enum value
  * \returns an appropriately modified index
  */
 template <const resampler_boundary_e tBoundary>
-NR_HOST_DEV int reg_applyBoundary(const int idx, const int bound) {
-  int bdyIdx = idx;
+NR_HOST_DEV int reg_applyBoundary(const int idx, const int bound);
 
-  switch (tBoundary) {
-  case resampler_boundary_e::CLAMPING:
-    bdyIdx = bdyIdx >= 0? bdyIdx : 0;
-    bdyIdx = bdyIdx < bound? bdyIdx : bound - 1;
-    break;
-
-  case resampler_boundary_e::REFLECTING: {
-    const int wrap_size = 2*bound - 2;
-
-    bdyIdx = bound - 1 - std::abs(bound - 1 - (bdyIdx%wrap_size + wrap_size)%wrap_size);
-    break;
-  }
-  }
-
-  return bdyIdx;
-}
-/* *************************************************************** */
 /**
  * \param bound upper bound on index
  * \tparam tBoundary boundary treatment enum value
  * \returns true if the argument index lies between 0 (incl) and bound (excl), or the index is guearanteed to be valid by virtue of the applied boundary treatment.
  */
 template <const resampler_boundary_e tBoundary, typename TIndex, typename TBound>
-NR_HOST_DEV bool reg_checkImageDimensionIndex(const TIndex index, const TBound bound) {
-  return resampler_boundary_e(tBoundary) == resampler_boundary_e::CLAMPING || resampler_boundary_e(tBoundary) == resampler_boundary_e::REFLECTING || (index >= 0 && index < bound);
-}
-/* *************************************************************** */
+NR_HOST_DEV bool reg_checkImageDimensionIndex(const TIndex index, const TBound bound);
+
 /** \returns the appropriate padding value for a given boundary treatment enum value */
 template <typename TVoxel>
-NR_HOST_DEV constexpr TVoxel reg_getPaddingValue(const resampler_boundary_e boundary) {
-  return boundary == resampler_boundary_e::ZEROPAD? TVoxel(0)
-    : (std::is_integral<TVoxel>::value? std::numeric_limits<TVoxel>::lowest() : std::numeric_limits<TVoxel>::quiet_NaN());
-}
+NR_HOST_DEV constexpr TVoxel reg_getPaddingValue(const resampler_boundary_e boundary);
+
+#include "resampler_boundary.tpp"
