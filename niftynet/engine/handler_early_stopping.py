eebd668564ed23162cb11282cb3a9443c3ef08fc
Thomas <ucabtmv@ucl.ac.uk>
2019-06-06 15:23:00 +0000

    pylint changes

--- niftynet/engine/handler_early_stopping.py
+++ niftynet/engine/handler_early_stopping.py
@@ -1,9 +1,8 @@
-from niftynet.engine.application_variables import CONSOLE
-from niftynet.engine.signal import ITER_FINISHED
-
 import tensorflow as tf
 import numpy as np
 from scipy.ndimage import median_filter
+
+from niftynet.engine.signal import ITER_FINISHED
 
 
 class EarlyStopper(object):
@@ -43,33 +42,44 @@ def compute_generalisation_loss(validation_his):
     return (last-min_val_loss)/(max_val_loss - min_val_loss)
 
 
-def check_should_stop(performance_history, patience,
-                      mode='mean', min_delta=0.03, kernel_size=5):
+def check_should_stop(performance_history, mode='mean', min_delta=0.03,
+                      kernel_size=5, k_splits=5):
     """
     This function takes in a mode, performance_history and patience and
     returns True if the application should stop early.
-    :param mode: {'mean', 'robust_mean', 'median', 'generalisation_loss', 'median_smoothing', 'validation_up'}
-           the default mode is 'mean'
-    :param performance_history: a list of size patience with the performance history
+    :param mode: {'mean', 'robust_mean', 'median', 'generalisation_loss',
+     'median_smoothing', 'validation_up'} the default mode is 'mean'
+    mean:
+        If your last loss is less than the average across the entire
+        performance history stop training
+    robust_mean:
+        Same as 'mean' but only loss values within 5th and 95th
+        percentile are considered
+    median:
+        As in mode='mean' but using the median
+    generalisation_loss:
+        Computes generalisation loss over the performance
+        history, and stops if it reaches an arbitrary threshold of 0.2.
+    validation_up:
+        This method check for performance increases in k sub-arrays of
+        length s, where k x s = patience. Because we cannot guarantee
+        patience to be divisible by both k and s, we define that k is
+        either 4 or 5, depending on which has the smallest remainder when
+        dividing.
+    :param performance_history: a list of size patience with the performance
+    history
     :param patience: see above
     :param min_delta: threshold for smoothness
     :param kernel_size: hyperparameter for median smoothing
+    :param k_splits: number of splits if using 'validation_up'
     :return:
     """
     if mode == 'mean':
-        """
-        If your last loss is less than the average across the entire 
-        performance history stop training
-        """
         performance_to_consider = performance_history[:-1]
-        tresh = np.mean(performance_to_consider)
-        should_stop = performance_history[-1] > tresh
+        thresh = np.mean(performance_to_consider)
+        should_stop = performance_history[-1] > thresh
 
     elif mode == 'robust_mean':
-        """
-        Same as 'mean' but only loss values within 5th and 95th percentile
-        are considered
-        """
         performance_to_consider = performance_history[:-1]
         perc = np.percentile(performance_to_consider, q=[5, 95])
         temp = []
@@ -79,45 +89,30 @@ def check_should_stop(performance_history, patience,
         should_stop = performance_history[-1] > np.mean(temp)
 
     elif mode == 'median':
-        """
-        As in mode='mean' but using the median
-        """
         performance_to_consider = performance_history[:-1]
         should_stop = performance_history[-1] > np.median(
             performance_to_consider)
 
     elif mode == 'generalisation_loss':
-        """
-        Computes generalisation loss over the performance history,
-        and stops if it reaches an arbitrary threshold of 0.2.
-        """
-
         value = compute_generalisation_loss(performance_history)
         should_stop = value > 0.2
 
     elif mode == 'median_smoothing':
         smoothed = median_filter(performance_history[:-1],
-                                  size=kernel_size)
+                                 size=kernel_size)
         gradient = np.gradient(smoothed)
-        tresholded = np.where(gradient < min_delta, 1, 0)
-        value = np.sum(tresholded) / len(gradient)
+        thresholded = np.where(gradient < min_delta, 1, 0)
+        value = np.sum(thresholded) / len(gradient)
         should_stop = value < 0.5
     elif mode == 'validation_up':
-        # Strip-based methods:
-        # These methods check for performance increases in k sub-arrays of
-        # length s, where k x s = patience. Because we cannot guarantee
-        # patience to be divisible by both k and s, we define that k is
-        # either 4 or 5, depending on which has the smallest remainder when
-        # dividing.
-        remainder = len(performance_history) % kernel_size
+        remainder = len(performance_history) % k_splits
         performance_to_consider = performance_history[remainder:]
-
-        strips = np.split(np.array(performance_to_consider), kernel_size)
-        GL_increase = []
+        strips = np.split(np.array(performance_to_consider), k_splits)
+        gl_increase = []
         for strip in strips:
-            GL = compute_generalisation_loss(strip)
-            GL_increase.append(GL >= min_delta)
-        should_stop = False not in GL_increase
+            gl = compute_generalisation_loss(strip)
+            gl_increase.append(gl >= min_delta)
+        should_stop = False not in gl_increase
     else:
         raise Exception('Mode: {} provided is not supported'.format(mode))
     return should_stop
--- tests/handler_early_stopping_test.py
+++ tests/handler_early_stopping_test.py
@@ -24,35 +24,31 @@ class EarlyStopperTest(tf.test.TestCase):
     def test_robust_mean(self):
         should_stop = check_should_stop(mode='robust_mean',
                                         performance_history=[1, 2, 1, 2, 1, 2,
-                                                             1, 200, -10, 1.4],
-                                        patience=10)
+                                                             1, 200, -10, 1.4])
         self.assertFalse(should_stop)
         should_stop = check_should_stop(mode='robust_mean',
                                         performance_history=[1, 2, 1, 2, 1, 2,
-                                                             1, 200, -10, 1.5],
-                                        patience=10)
+                                                             1, 200, -10, 1.5])
         self.assertTrue(should_stop)
 
     def test_median(self):
         should_stop = check_should_stop(mode='median',
-                                        performance_history=[1, 2, 1, 2, 1, 2, 1, 2, 3],
-                                        patience=3)
+                                        performance_history=[1, 2, 1, 2, 1, 2,
+                                                             1, 2, 3])
         self.assertTrue(should_stop)
         should_stop = check_should_stop(mode='median',
-                                        performance_history=[1, 2, 1, 2, 1, 2, 1, 2, 3, 0],
-                                        patience=3)
+                                        performance_history=[1, 2, 1, 2, 1, 2,
+                                                             1, 2, 3, 0])
         self.assertFalse(should_stop)
 
     def test_generalisation_loss(self):
         should_stop = check_should_stop(mode='generalisation_loss',
                                         performance_history=[1, 2, 1, 2, 1,
-                                                             2, 1, 2, 3],
-                                        patience=6)
+                                                             2, 1, 2, 3])
         self.assertTrue(should_stop)
         should_stop = check_should_stop(mode='generalisation_loss',
                                         performance_history=[1, 2, 1, 2, 3,
-                                                             2, 1, 2, 1],
-                                        patience=6)
+                                                             2, 1, 2, 1])
         self.assertFalse(should_stop)
 
     def test_validation_up(self):
@@ -62,18 +58,15 @@ class EarlyStopperTest(tf.test.TestCase):
             data.extend(np.arange(2, 10)[::-1])
         should_stop = check_should_stop(mode='validation_up',
                                         performance_history=np.arange(0,
-                                                                      20) / 10,
-                                        patience=8)
+                                                                      20) / 10)
         self.assertTrue(should_stop)
         should_stop = check_should_stop(mode='validation_up',
                                         performance_history=np.arange(
-                                            0, 20)[::-1] / 10,
-                                        patience=8)
+                                            0, 20)[::-1] / 10)
         self.assertFalse(should_stop)
 
         should_stop = check_should_stop(mode='validation_up',
                                         performance_history=data,
-                                        patience=30,
                                         min_delta=0.2)
         self.assertFalse(should_stop)
 
@@ -82,26 +75,23 @@ class EarlyStopperTest(tf.test.TestCase):
         for i in range(10):
             data.extend(np.arange(0, 8))
             data.extend(np.arange(1, 9)[::-1])
-        should_stop = check_should_stop(mode='median_smoothing',
-                                        performance_history=np.arange(0,20) / 10,
-                                        patience=8)
+        should_stop = \
+            check_should_stop(mode='median_smoothing',
+                              performance_history=np.arange(0, 20) / 10)
         self.assertTrue(should_stop)
         should_stop = check_should_stop(mode='median_smoothing',
                                         performance_history=np.arange(
-                                            0, 20)[::-1] / 10,
-                                        patience=8)
+                                            0, 20)[::-1] / 10)
         self.assertFalse(should_stop)
 
         should_stop = check_should_stop(mode='median_smoothing',
-                                        performance_history=data,
-                                        patience=30)
+                                        performance_history=data)
         self.assertFalse(should_stop)
 
     def test_weird_mode(self):
         with self.assertRaises(Exception):
             check_should_stop(mode='adslhfjdkas',
-                              performance_history=[1,2,3,4,5,6,7,8,9],
-                              patience=3)
+                              performance_history=[1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 
 if __name__ == "__main__":
