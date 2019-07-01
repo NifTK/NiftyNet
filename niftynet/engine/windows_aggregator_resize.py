85a122d0d8a31c412aee42bd6182ad5bd1633878
Wenqi Li <wenqil@nvidia.com>
2019-07-01 11:00:14 +0100

    fixes pylint

--- niftynet/engine/windows_aggregator_resize.py
+++ niftynet/engine/windows_aggregator_resize.py
@@ -3,14 +3,14 @@
 Windows aggregator resize each item
 in a batch output and save as an image.
 """
-from __future__ import absolute_import, print_function, division
+from __future__ import absolute_import, division, print_function
 
 import os
 
+import numpy as np
 import tensorflow as tf
 
 import niftynet.io.misc_io as misc_io
-import numpy as np
 from niftynet.engine.sampler_resize_v2 import zoom_3d
 from niftynet.engine.windows_aggregator_base import ImageWindowsAggregator
 from niftynet.layer.discrete_label_normalisation import \
@@ -24,6 +24,7 @@ class ResizeSamplesAggregator(ImageWindowsAggregator):
     window and save as a new image volume. Multiple output image can be
     proposed and csv output can be performed as well
     """
+
     def __init__(self,
                  image_reader,
                  name='image',
@@ -92,27 +93,26 @@ class ResizeSamplesAggregator(ImageWindowsAggregator):
                             window[i] = np.expand_dims(window[i], 1)
                         elif n_samples == 1 and window[i].shape[0] != n_samples:
                             window[i] = np.expand_dims(window[i], 0)
-                        window_save = np.asarray(np.squeeze(
-                            window[i][batch_id, ...]))
+                        window_save = np.asarray(
+                            np.squeeze(window[i][batch_id, ...]))
                         try:
                             assert window_save.ndim <= 2
                         except (TypeError, AssertionError):
-                            tf.logging.error(
-                                "The output you are trying to "
-                                "save as csv is more than "
-                                "bidimensional. Did you want "
-                                "to save an image instead? "
-                                "Put the keyword window "
-                                "in the output dictionary"
-                                " in your application file")
+                            tf.logging.error("The output you are trying to "
+                                             "save as csv is more than "
+                                             "bidimensional. Did you want "
+                                             "to save an image instead? "
+                                             "Put the keyword window "
+                                             "in the output dictionary"
+                                             " in your application file")
                         while window_save.ndim < 2:
                             window_save = np.expand_dims(window_save, 0)
                         window_loc = window_save
                         self.csv_out[i] = self._initialise_empty_csv(
                             n_channel=window_save.shape[-1])
 
-                    self.csv_out[i] = np.concatenate([self.csv_out[i],
-                                                      window_loc], 0)
+                    self.csv_out[i] = np.concatenate(
+                        [self.csv_out[i], window_loc], 0)
 
             self._save_current_image()
             self._save_current_csv()
@@ -120,16 +120,16 @@ class ResizeSamplesAggregator(ImageWindowsAggregator):
         return True
 
     def _initialise_image_shape(self, image_id, n_channels):
-        '''
+        """
         Return the shape of the empty image to be saved
         :param image_id: index to find the appropriate input image from the
         reader
         :param n_channels: number of channels of the image
         :return:  shape of the empty image
-        '''
+        """
         self.image_id = image_id
         spatial_shape = self.input_image[self.name].shape[:3]
-        output_image_shape = spatial_shape + (1, n_channels,)
+        output_image_shape = spatial_shape + (1, n_channels)
         empty_image = np.zeros(output_image_shape, dtype=np.bool)
         for layer in self.reader.preprocessors:
             if isinstance(layer, PadLayer):
@@ -137,19 +137,18 @@ class ResizeSamplesAggregator(ImageWindowsAggregator):
         return empty_image.shape
 
     def _save_current_image(self):
-        '''
+        """
         Loop through the dictionary of images output and resize and reverse
         the preprocessing prior to saving
         :return:
-        '''
+        """
         if self.input_image is None:
             return
 
         self.current_out = {}
         for i in self.image_out:
             resize_to_shape = self._initialise_image_shape(
-                image_id=self.image_id,
-                n_channels=self.image_out[i].shape[-1])
+                image_id=self.image_id, n_channels=self.image_out[i].shape[-1])
             window_shape = resize_to_shape
             current_out = self.image_out[i]
             while current_out.ndim < 5:
@@ -157,19 +156,21 @@ class ResizeSamplesAggregator(ImageWindowsAggregator):
             if self.window_border and any([b > 0 for b in self.window_border]):
                 np_border = self.window_border
                 while len(np_border) < 5:
-                    np_border = np_border + (0,)
-                np_border = [(b,) for b in np_border]
+                    np_border = np_border + (0, )
+                np_border = [(b, ) for b in np_border]
                 current_out = np.pad(current_out, np_border, mode='edge')
             image_shape = current_out.shape
             zoom_ratio = \
                 [float(p) / float(d) for p, d in zip(window_shape, image_shape)]
             image_shape = list(image_shape[:3]) + [1, image_shape[-1]]
-            print(np.sum(self.image_out[i]), " is sum of image out %s before"
-                  % i)
+            print(
+                np.sum(self.image_out[i]),
+                " is sum of image out %s before" % i)
             current_out = np.reshape(current_out, image_shape)
-            current_out = zoom_3d(image=current_out,
-                                  ratio=zoom_ratio,
-                                  interp_order=self.output_interp_order)
+            current_out = zoom_3d(
+                image=current_out,
+                ratio=zoom_ratio,
+                interp_order=self.output_interp_order)
             self.current_out[i] = current_out
 
         for layer in reversed(self.reader.preprocessors):
@@ -182,41 +183,36 @@ class ResizeSamplesAggregator(ImageWindowsAggregator):
                     self.image_out[i], _ = layer.inverse_op(self.image_out[i])
         subject_name = self.reader.get_subject_id(self.image_id)
         for i in self.image_out:
-            print(np.sum(self.image_out[i]), " is sum of image out %s after"
-                  % i)
+            print(
+                np.sum(self.image_out[i]), " is sum of image out %s after" % i)
         for i in self.image_out:
             filename = "{}_{}_{}.nii.gz".format(i, subject_name, self.postfix)
             source_image_obj = self.input_image[self.name]
-            misc_io.save_data_array(self.output_path,
-                                    filename,
-                                    self.current_out[i],
-                                    source_image_obj,
+            misc_io.save_data_array(self.output_path, filename,
+                                    self.current_out[i], source_image_obj,
                                     self.output_interp_order)
             self.log_inferred(subject_name, filename)
         return
 
     def _save_current_csv(self):
-        '''
+        """
         Save all csv output present in the dictionary of csv_output.
         :return:
-        '''
+        """
         if self.input_image is None:
             return
         subject_name = self.reader.get_subject_id(self.image_id)
         for i in self.csv_out:
             filename = "{}_{}_{}.csv".format(i, subject_name, self.postfix)
-            misc_io.save_csv_array(self.output_path,
-                                   filename,
-                                   self.csv_out[i])
+            misc_io.save_csv_array(self.output_path, filename, self.csv_out[i])
             self.log_inferred(subject_name, filename)
         return
 
     def _initialise_empty_csv(self, n_channel):
-        '''
+        """
         Initialise the array to be saved as csv as a line of zeros according
         to the number of elements to be saved
         :param n_channel:
         :return:
-        '''
+        """
         return np.zeros([1, n_channel])
-    
